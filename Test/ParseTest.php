751cb208985c9a6ea3799ca470e16dc5062405b1
remco van der Velde <remco.pc@outlook.com>
2024-09-11 20:45:18 +0200

    rename to RaXon

--- Test/ParseTest.php
+++ Test/ParseTest.php
@@ -8,7 +8,7 @@ use Raxon\App;
 
 test('parse', function () {
     $app = App::instance();
-    $parse = new Parse($app, $app->data(), $app->flags(), $app->options());
+    $parse = new Parse($app, $app->data(), App::flags($app), App::options($app));
 
     $string = '{{config(\'project.dir.root\')}}';
     $compile = $parse->compile($string);

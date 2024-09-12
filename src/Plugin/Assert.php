e02baee7f1b41a04c268783af1f20e924e01bc4a
remco van der Velde <remco.pc@outlook.com>
2024-09-12 18:55:05 +0200

    Plugin config

---
+++ src/Plugin/Assert.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * @package Plugin\Modifier
+ * @author Remco van der Velde
+ * @since 2024-08-19
+ * @license MIT
+ * @version 1.0
+ * @changeLog
+ *    - all
+ */
+namespace Plugin;
+
+trait Assert {
+
+    protected function assert(mixed $assertion, Throwable|string|null $description=null): bool
+    {
+        return assert($assertion, $description);
+    }
+
+}

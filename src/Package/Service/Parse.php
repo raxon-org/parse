65f1ff188e2979dc45b5e72ba708d337f640189d
remco van der Velde <remco.pc@outlook.com>
2024-09-14 15:15:41 +0200

    Plugin require

--- src/Package/Service/Parse.php
+++ src/Package/Service/Parse.php
@@ -251,12 +251,14 @@ class Parse
             File::touch($url_php, $mtime);
         }
         if($url_php){
+            $pre_require = microtime(true);
             require_once $url_php;
             $run = $options->namespace . '\\' . $options->class;
             $main = new $run($object, $this, $data, $flags, $options);
             $result = $main->run();
             $microtime = microtime(true);
-            $duration_parse = round(($microtime - $start) * 1000, 2) . 'ms';
+            $duration_require = round(($microtime - $pre_require) * 1000, 2) . ' ms';
+            $duration_parse = round(($microtime - $start) * 1000, 2) . ' ms';
             $duration_script = round(($microtime - $object->config('time.start')) * 1000, 2) . 'ms';
             $microtime_explode = explode('.', $microtime);
             if(property_exists($options, 'duration')){

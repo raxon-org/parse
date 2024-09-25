c5df001a0382143dcfda7d4306def74f8ec6abfb
remco van der Velde <remco.pc@outlook.com>
2024-09-25 17:58:05 +0200

    testing list

--- src/Package/Service/Token.php
+++ src/Package/Service/Token.php
@@ -935,6 +935,7 @@ class Token
         if($cache->has($hash)){
             $input = $cache->get($hash);
         } else {
+            d($input);
             $input = Symbol::define($object, $flags, $options, $input);
             $input = Cast::define($object, $flags, $options, $input);
             $input = Method::define($object, $flags, $options, $input);

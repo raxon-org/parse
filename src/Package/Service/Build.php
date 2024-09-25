762afe8c58d4b8ccb5492ed75ba29ebf0f789064
remco van der Velde <remco.pc@outlook.com>
2024-09-25 19:28:22 +0200

    testing list

--- src/Package/Service/Build.php
+++ src/Package/Service/Build.php
@@ -1117,6 +1117,7 @@ class Build
             if(!is_array($record)){
                 continue;
             }
+            d($record);
             if(
                 array_key_exists('is_single_quoted', $record) &&
                 array_key_exists('execute', $record) &&

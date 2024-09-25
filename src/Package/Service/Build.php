f5893a3d450b446c54e5c1c11fa8e9edaae18c14
remco van der Velde <remco.pc@outlook.com>
2024-09-25 22:03:28 +0200

    empty array test

--- src/Package/Service/Build.php
+++ src/Package/Service/Build.php
@@ -791,7 +791,7 @@ class Build
                 $method_value[] = '}';
                 if($key){
                     $method_value[] = 'foreach(' . $from . ' as ' . $key . ' => ' . $value . '){';
-                    $foreach_value = '$data->set(\'' . $foreach_key['name'] . '\', ' . $key . ');' . PHP_EOL;
+                    $foreach_value = '$data->set(\'' . $foreach_key . '\', ' . $key . ');' . PHP_EOL;
                     $foreach_value .= '$data->set(\'' . $foreach_value['name'] . '\', ' . $value . ');';
                 } else {
                     $method_value[] = 'foreach(' . $from . ' as ' . $value . '){';

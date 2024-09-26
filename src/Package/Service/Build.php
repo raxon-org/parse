cf52223bc7441a011b7aecf5cc47e772473423ed
remco van der Velde <remco.pc@outlook.com>
2024-09-26 11:31:11 +0200

    empty array test

--- src/Package/Service/Build.php
+++ src/Package/Service/Build.php
@@ -758,7 +758,7 @@ class Build
                 }
                 d($foreach_from);
                 d($foreach_key);
-                d($foreach_value);
+                ddd($foreach_value);
                 $foreach_from = Build::value($object, $flags, $options, $record, $value);
                 $from = Core::uuid_variable();
                 $value = Core::uuid_variable();

fd0eb32a5224a70c3b06205e7c12b7921818bdcc
remco van der Velde <remco.pc@outlook.com>
2024-09-28 22:04:39 +0200

    empty array test

--- src/Package/Service/Token.php
+++ src/Package/Service/Token.php
@@ -944,6 +944,7 @@ class Token
         if($cache->has($hash)){
             $input = $cache->get($hash);
         } else {
+            d($input);
             $input = Symbol::define($object, $flags, $options, $input);
             $input = Cast::define($object, $flags, $options, $input);
             $input = Method::define($object, $flags, $options, $input);

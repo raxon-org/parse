b53bd5d294d0c60b8db64b96f2d0d43d87bb8ecc
remco van der Velde <remco.pc@outlook.com>
2024-09-29 23:22:05 +0200

    plugin d

--- src/Package/Test/Test2.php
+++ src/Package/Test/Test2.php
@@ -8,7 +8,7 @@
  * $list = 0, list is empty
  * $list = 1, list is not empty
  * $list = '', list is empty
- * $list = '0', List is not empty', list is empty in template engine
+ * $list = '0', List is  empty'
  */
 $list = '0';
 var_dump($list);
@@ -16,7 +16,7 @@ $counter = null;
 if($counter === 0){
 
 }
-elseif($list){
+elseif('0'){
     echo 'List is not empty';
 }
 else{

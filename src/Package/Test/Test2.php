c51cc0924728f881544a84cf9521b2d4948b24b7
remco van der Velde <remco.pc@outlook.com>
2024-09-29 22:03:19 +0200

    elseif failures

--- src/Package/Test/Test2.php
+++ src/Package/Test/Test2.php
@@ -1,4 +1,15 @@
 <?php
+/**
+ * same behaviour as in the template engine
+ * $list = [], list is empty
+ * $list = (object) [], list is not empty
+ * $list = false, list is empty
+ * $list = null, list is empty
+ * $list = 0, list is empty
+ * $list = 1, list is not empty
+ * $list = '', list is empty
+ * $list = '0', list is not empty
+ */
 $list = (object) [];
 $counter = null;
 if($counter === 0){
---
+++ src/Package/Test/Test66.tpl
@@ -0,0 +1,13 @@
+{{$counter = 1}}
+{{$elseif = '0'}}
+{{while(true)}}
+    {{$counter}}
+    {{$counter++}}
+    {{if($counter > 10)}}
+        {{break()}}
+    {{elseif($elseif)}}
+        {{$counter}} onder de 10
+    {{else}}
+        {{$counter}} boven de 5
+    {{/if}}
+{{/while}}

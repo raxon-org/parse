e1d5bf8a253a57341d55a0b6de1f60d8c7db20a1
remco van der Velde <remco.pc@outlook.com>
2024-10-01 22:12:16 +0200

    make emptyy arguments null by default

--- src/Package/Service/Build.php
+++ src/Package/Service/Build.php
@@ -1370,7 +1370,9 @@ class Build
                 if($argument_count === 3){
                     foreach($record['method']['argument'] as $nr => $argument){
                         $value = Build::value($object, $flags, $options, $record, $argument);
-                        d($value);
+                        if(strtolower($value) === 'null'){
+                            $value = '';
+                        }
                         $method_value[] = $value . ';';
                     }
                     $method_value[3] = substr($method_value[3], 0, -1);

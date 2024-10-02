75aa48c17ec2ccb82dc9def63fb99e2730891639
remco van der Velde <remco.pc@outlook.com>
2024-10-02 17:31:15 +0200

    make emptyy arguments null by default

--- src/Package/Service/Parse.php
+++ src/Package/Service/Parse.php
@@ -240,7 +240,7 @@ class Parse
                     $object->config('extension.php')
                 ;
             }
-            ddd($url_json);
+            d($url_json);
             $document = Build::create($object, $flags, $options, $token);
             File::write($url_php, implode(PHP_EOL, $document));
             File::permission(

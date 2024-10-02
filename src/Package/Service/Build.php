4ad2c574d206cd6f636809e5f4e2a5b00f0f7556
remco van der Velde <remco.pc@outlook.com>
2024-10-02 21:15:05 +0200

    mb_strtolower / mb_strtoupper replace

--- src/Package/Service/Build.php
+++ src/Package/Service/Build.php
@@ -539,6 +539,7 @@ class Build
                             $method = $object->config('package.raxon/parse.build.state.block.record');
                             $plugin = $object->config('package.raxon/parse.build.state.block.plugin');
                             $data[] = '$block = ob_get_clean();';
+                            $data[] = 'ddd($block);';
                             $data[] = '$block = Core::object($block, Core::OBJECT_OBJECT);';
                             $data[] = '$block = $parse->compile($block, $data);';
                             $argument = [];

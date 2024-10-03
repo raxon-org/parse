4a38cb87b362c7296348cae2c1ed7a930e211237
remco van der Velde <remco.pc@outlook.com>
2024-10-03 18:28:35 +0200

    mb_strtolower / mb_strtoupper replace

---
+++ src/Package/Test/Test4.php
@@ -0,0 +1,10 @@
+<?php
+echo 'Leave "name" empty if finished.' . PHP_EOL;
+fwrite(STDOUT, 'Leave "name" empty if finished.' . PHP_EOL);
+fflush(STDOUT);
+$input = trim(fgets(STDIN));
+echo 'Leave "name" empty if finished.' . PHP_EOL;
+$input = trim(fgets(STDIN));
+echo 'Leave "name" empty if finished.' . PHP_EOL;
+$input = trim(fgets(STDIN));
+echo $input;

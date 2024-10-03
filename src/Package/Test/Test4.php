8fb323c0ccb71c41c6343cb07aab90c14734add7
remco van der Velde <remco.pc@outlook.com>
2024-10-03 18:31:32 +0200

    mb_strtolower / mb_strtoupper replace

--- src/Package/Test/Test4.php
+++ src/Package/Test/Test4.php
@@ -1,10 +1,18 @@
 <?php
-echo 'Leave "name" empty if finished.' . PHP_EOL;
-fwrite(STDOUT, 'Leave "name" empty if finished.' . PHP_EOL);
-fflush(STDOUT);
-$input = trim(fgets(STDIN));
-echo 'Leave "name" empty if finished.' . PHP_EOL;
-$input = trim(fgets(STDIN));
-echo 'Leave "name" empty if finished.' . PHP_EOL;
-$input = trim(fgets(STDIN));
-echo $input;
+
+$item['Node']['property'] = testfunction();
+
+var_dump($item);
+
+function testfunction(){
+    echo 'Leave "name" empty if finished.' . PHP_EOL;
+    fwrite(STDOUT, 'Leave "name" empty if finished.' . PHP_EOL);
+    fflush(STDOUT);
+    $input = trim(fgets(STDIN));
+    echo 'Leave "name" empty if finished.' . PHP_EOL;
+    $input = trim(fgets(STDIN));
+    echo 'Leave "name" empty if finished.' . PHP_EOL;
+    $input = trim(fgets(STDIN));
+    return $input;
+}
+

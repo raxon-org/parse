b68304ba08d0543b73e563434dd2c14bf82fe759
remco van der Velde <remco.pc@outlook.com>
2024-10-03 19:22:40 +0200

    Plugin object

---
+++ src/Plugin/Plugin.Object.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * @package Plugin\Modifier
+ * @author Remco van der Velde
+ * @since 2024-08-19
+ * @license MIT
+ * @version 1.0
+ * @changeLog
+ *    - all
+ */
+namespace Plugin;
+
+use Raxon\Module\Core;
+
+use Raxon\Exception\ObjectException;
+
+trait Plugin_object {
+
+    /**
+     * @throws ObjectException
+     */
+    protected function plugin_object($input=null, $output=null, $type=null){
+        return Core::object($input, $output, $type);
+    }
+
+}

3f3d4cff2a81ba01b99b41573a0b803a9e0ba3de
remco van der Velde <remco.pc@outlook.com>
2024-10-04 15:02:29 +0200

    framework

--- src/Package/Service/Build.php
+++ src/Package/Service/Build.php
@@ -2782,6 +2782,7 @@ class Build
                 array_key_exists('type', $record) &&
                 $record['type'] === 'method'
             ){
+                d($record);
                 $plugin = Build::plugin($object, $flags, $options, $tag, str_replace('.', '_', $record['method']['name']));
                 $method_value = '$this->' . $plugin . '(' . PHP_EOL;
                 if(

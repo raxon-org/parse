9f8611ffcaeaa81feb86f89eff7a8c9bad1c35d8
remco van der Velde <remco.pc@outlook.com>
2024-10-04 19:16:04 +0200

    $framework.config()

--- src/Package/Service/Build.php
+++ src/Package/Service/Build.php
@@ -2134,7 +2134,7 @@ class Build
                 $argument[$argument_nr] = $value;
             }
             $before[] = 'try {';
-            $before[] = $uuid_methods . ' = get_class_methods(' . $class_name . ');';
+            $before[] = $uuid_methods . ' = get_class_methods(' . str_replace('::', '\\', $class_name) . ');';
             $before[] = '}';
             $before[] = 'catch(Exception | LocateException $exception){';
             $before[] = 'throw new TemplateException(\'' . $record['tag'] . PHP_EOL . 'On line: ' . $record['line']  . ', column: ' . $record['column']['start'] . ' in source: ' . $source . '.\', 0, $exception);';

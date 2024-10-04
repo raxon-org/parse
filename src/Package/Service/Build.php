6e3d9464632a6fa853a8b9bc2171465ac8af1a69
remco van der Velde <remco.pc@outlook.com>
2024-10-04 19:17:32 +0200

    $framework.config()

--- src/Package/Service/Build.php
+++ src/Package/Service/Build.php
@@ -2124,8 +2124,9 @@ class Build
             $method = str_replace('.', '_', $method);
             $explode = explode('::', $method);
             $function = array_pop($explode);
+            $method = implode('\\', $explode);
             $class_name = $method;
-            $method = implode('\\', $explode) . '::' . $function;
+            $method .= '::' . $function;
             $uuid = Core::uuid_variable();
             $uuid_methods = Core::uuid_variable();
             $argument = $record['variable']['value']['array'][0]['method']['argument'] ?? [];
@@ -2134,7 +2135,7 @@ class Build
                 $argument[$argument_nr] = $value;
             }
             $before[] = 'try {';
-            $before[] = $uuid_methods . ' = get_class_methods(' . str_replace('::', '\\', $class_name) . ');';
+            $before[] = $uuid_methods . ' = get_class_methods(' . $class_name . ');';
             $before[] = '}';
             $before[] = 'catch(Exception | LocateException $exception){';
             $before[] = 'throw new TemplateException(\'' . $record['tag'] . PHP_EOL . 'On line: ' . $record['line']  . ', column: ' . $record['column']['start'] . ' in source: ' . $source . '.\', 0, $exception);';

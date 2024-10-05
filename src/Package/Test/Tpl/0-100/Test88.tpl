ba2017532cbd966203c1540361d7cbcd14786e32
remco van der Velde <remco.pc@outlook.com>
2024-10-06 00:11:59 +0200

    sets

---
+++ src/Package/Test/Tpl/0-100/Test88.tpl
@@ -0,0 +1,4 @@
+{{$framework.test = framework()}}
+{{$app = ::Raxon::App::instance()}}
+{{$time.duration = ($app->config('time.start') - $framework.test->config('time.start')) * 1000 + ' ms'}}
+{{breakpoint($time)}}
---
+++ src/Plugin/Breakpoint.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * @package Plugin\Modifier
+ * @author Remco van der Velde
+ * @since 2024-08-19
+ * @license MIT
+ * @version 1.0
+ * @changeLog
+ *    - all
+ */
+namespace Plugin;
+
+use Raxon\Exception\ObjectException;
+use Raxon\Module\Core;
+
+trait Breakpoint {
+
+    /**
+     * @throws ObjectException
+     */
+    public function breakpoint($value): void
+    {
+        $object = $this->object();
+        $tag = Core::object($object->config('package.raxon/parse.build.state.tag'), Core::OBJECT);
+        if(property_exists($tag, 'source')){
+            if(
+                property_exists($tag, 'line') &&
+                is_object($tag->line) &&
+                property_exists($tag->line, 'start')
+            ){
+                echo $tag->source . ':' . $tag->line->start . PHP_EOL;
+            } else {
+                echo $tag->source . ':' . $tag->line . PHP_EOL;
+            }
+        }
+        breakpoint($value);
+    }
+
+}

6e7fa415d9aa349e7e162a0a3c67997c2549a7f5
remco van der Velde <remco.pc@outlook.com>
2024-10-06 17:11:21 +0200

    sets

--- src/Package/Service/Build.php
+++ src/Package/Service/Build.php
@@ -2309,6 +2309,7 @@ class Build
             }
         } else {
             d($record);
+            /*
             foreach($record['variable']['value']['array'] as $record_variable_array_nr => $record_variable_array){
                 if(
                     array_key_exists('type', $record_variable_array) &&
@@ -2316,9 +2317,9 @@ class Build
                     array_key_exists('name', $record_variable_array['method']) &&
                     is_array($record_variable_array['method']['name'])
                 ){
-                    $
                 }
             }
+            */
 
 
             $value = Build::value($object, $flags, $options, $record, $record['variable']['value'],$is_set);

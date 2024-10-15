03dd69237b075d85400119d7abe63c76be18376c
remco van der Velde <remco.pc@outlook.com>
2024-10-15 20:40:01 +0200

    test for with assignment

--- src/Package/Service/Build.php
+++ src/Package/Service/Build.php
@@ -3440,7 +3440,7 @@ class Build
                         break;
                         case '+=':
                             $variable_value = Build::value($object, $flags, $options, $tag, $record['variable']['value'], $is_set);
-                            $value .= '$data->set(\'' . $record['variable']['name'] . '\', ' .  '$this->value_plus($data->get(\'' . $record['variable']['name'] . '\', ' .  $variable_value . ')))';
+                            $value .= '$data->set(\'' . $record['variable']['name'] . '\', ' .  '$this->value_plus($data->get(\'' . $record['variable']['name'] . '\'), ' .  $variable_value . '))';
                             break;
                         case '++':
                             $value .= '$data->set(\'' . $record['variable']['name'] . '\', ' .  '$this->value_plus_plus($data->get(\'' . $record['variable']['name'] . '\')))';

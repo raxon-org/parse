b48fbdd69ff8ff251a5085fbbc74c46a61968a0f
remco van der Velde <remco.pc@outlook.com>
2024-10-16 21:01:33 +0200

    test for with assignment

--- src/Package/Service/Tag.php
+++ src/Package/Service/Tag.php
@@ -301,7 +301,141 @@ class Tag
                     $char === '}' &&
                     $previous === '}'
                 ){
+                    $tag .= $char;
                     ddd($tag);
+                    if($tag){
+                        if(mb_strlen($text) > 0){
+                            $text = mb_substr($text, 0, -1);
+                        }
+                        if($is_comment === false){
+                            $tag .= $char;
+                        }
+                        $column[$line]++;
+                        if($text !== ''){
+                            $explode = explode("\n", $text);
+                            $count = count($explode);
+                            $explode_tag = explode("\n", $tag);
+                            if($count > 1){
+                                $length_start = mb_strlen($explode[0]);
+                                $record = [
+                                    'text' => $text,
+                                    'is_multiline' => true,
+                                    'line' => [
+                                        'start' => $line - $count + 1,
+                                        'end' => $line
+                                    ],
+                                    'length' => [
+                                        'start' => $length_start,
+                                        'end' => mb_strlen($explode[$count - 1])
+                                    ],
+                                    'column' => [
+                                        ($line - $count + 1) => [
+                                            'start' => $column[$line - $count + 1] - $length_start,
+                                            'end' => $column[$line - $count + 1]
+                                        ],
+                                        $line => [
+                                            'start' => $column[$line] - mb_strlen($explode[$count - 1]) - mb_strlen($explode_tag[0]),
+                                            'end' => $column[$line] - mb_strlen($explode_tag[0])
+                                        ]
+                                    ]
+                                ];
+                                if(empty($tag_list[$line - $count + 1])){
+                                    $tag_list[$line - $count + 1] = [];
+                                }
+                                $tag_list[$line - $count + 1][] = $record;
+                            } else {
+                                $length_start = mb_strlen($explode[0]);
+                                $record = [
+                                    'text' => $text,
+                                    'line' => $line,
+                                    'length' => $length_start,
+                                    'column' => [
+                                        'start' => $column[$line] - $length_start - mb_strlen($explode_tag[0]),
+                                        'end' => $column[$line] - mb_strlen($explode_tag[0])
+                                    ]
+                                ];
+                                if(empty($tag_list[$line])){
+                                    $tag_list[$line] = [];
+                                }
+                                $tag_list[$line][] = $record;
+                            }
+                        }
+                        $text = '';
+                        $explode = explode("\n", $tag);
+                        $count = count($explode);
+                        if($count > 1){
+                            $content = trim(mb_substr($tag, 2, -2));
+                            $length_start = mb_strlen($explode[0]);
+                            $record = [
+                                'tag' => $tag,
+                                'is_multiline' => true,
+                                'line' => [
+                                    'start' => $line - $count + 1,
+                                    'end' => $line
+                                ],
+                                'length' => [
+                                    'start' => $length_start,
+                                    'end' => mb_strlen($explode[$count - 1])
+                                ],
+                                'column' => [
+                                    ($line - $count + 1) => [
+                                        'start' => $column[$line - $count + 1] - $length_start,
+                                        'end' => $column[$line - $count + 1]
+                                    ],
+                                    $line => [
+                                        'start' => $column[$line] - mb_strlen($explode[$count - 1]),
+                                        'end' => $column[$line]
+                                    ]
+                                ]
+                            ];
+                            if(empty($tag_list[$line - $count + 1])){
+                                $tag_list[$line - $count + 1] = [];
+                            }
+                            $tag_list[$line - $count + 1][] = $record;
+                        } else {
+                            $length_start = mb_strlen($explode[0]);
+                            $record = [
+                                'tag' => $tag,
+                                'line' => $line,
+                                'length' => $length_start,
+                                'column' => [
+                                    'start' => $column[$line] - $length_start,
+                                    'end' => $column[$line]
+                                ]
+                            ];
+                            $content = trim(mb_substr($tag, 2, -2));
+                            if(mb_strtoupper(mb_substr($content, 0, 3)) === 'R3M'){
+                                $record['is_header'] = true;
+                                $record['content'] = $content;
+                            }
+                            elseif(mb_strtoupper(mb_substr($content, 0, 3)) === 'RAX'){
+                                $record['is_header'] = true;
+                                $record['content'] = $content;
+                            }
+                            elseif(
+                                mb_strtoupper($content) === 'LITERAL' ||
+                                $is_literal === true
+                            ){
+                                $is_literal = true;
+                                $record['is_literal'] = true;
+                                $record['is_literal_start'] = true;
+                            }
+                            elseif(
+                                mb_strtoupper($content) === '/LITERAL' ||
+                                $is_literal === true
+                            ){
+                                $is_literal = false;
+                                $record['is_literal'] = true;
+                                $record['is_literal_end'] = true;
+                            }
+                            if(empty($tag_list[$line])){
+                                $tag_list[$line] = [];
+                            }
+                            $tag_list[$line][] = $record;
+                        }
+                        $tag = false;
+                        $column[$line]--;
+                    }
                 }
 
 

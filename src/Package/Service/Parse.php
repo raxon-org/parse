f8eff1625bf518b45c8ab9265045e8488b843019
remco van der Velde <remco.pc@outlook.com>
2024-10-17 11:50:38 +0200

    test block.data

--- src/Package/Service/Parse.php
+++ src/Package/Service/Parse.php
@@ -323,9 +323,7 @@ class Parse
                     $options->class = Parse::class_name($object, $options->source);
                     $data->set('this.' . $object->config('package.raxon/parse.object.this.property'), $key);
                     $data->set('this.' . $object->config('package.raxon/parse.object.this.attribute'), $key);
-                    breakpoint($value);
                     $input->{$key} = $this->compile($value, $data);
-                    breakpoint($input->{$key});
                     $options->source = $temp_source;
                     $options->class = $temp_class;
                 }

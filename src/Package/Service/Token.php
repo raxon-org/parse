15fa51f1aa1b1e9070f343db0446cf64e670c53f
remco van der Velde <remco.pc@outlook.com>
2024-10-18 14:58:35 +0200

    debugging test 134

--- src/Package/Service/Token.php
+++ src/Package/Service/Token.php
@@ -69,10 +69,8 @@ class Token
         }
         if($tags === false){
             $tags = Tag::define($object, $flags, $options, $input);
-            breakpoint($tags);
             $tags = Tag::remove($object, $flags, $options, $tags);
             $tags = Token::abstract_syntax_tree($object, $flags, $options, $tags);
-            breakpoint($tags);
             $is_new = true;
         }
         if(

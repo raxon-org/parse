77fe1b341b2d6d1dae5bb57bd81dc7ac7660ec03
remco van der Velde <remco.pc@outlook.com>
2024-10-20 00:09:42 +0200

    absolute class static method call

--- src/Package/Test/Php/1-100/Test5.php
+++ src/Package/Test/Php/1-100/Test5.php
@@ -1,2 +1,15 @@
 <?php
-
+$test = [
+    'This',
+    'is',
+    'a',
+    'test',
+    'test'
+];
+asort($test, SORT_NATURAL);
+foreach($test as $nr => $value) {
+    $array[] = $value;
+    $index[] = $nr;
+}
+var_dump($array);
+var_dump($index);

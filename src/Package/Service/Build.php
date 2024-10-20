20f95d4fa22a40eda7327efae910a4b58d446ca2
remco van der Velde <remco.pc@outlook.com>
2024-10-20 22:21:55 +0200

    absolute class static method call

--- src/Package/Service/Build.php
+++ src/Package/Service/Build.php
@@ -2366,9 +2366,6 @@ class Build
                         '(';
                     $method_value .= Build::argument($object, $flags, $options, $record, $before, $after);
                     $method_value .= ');';
-//                    d($method_value);
-//                    d($after);
-//                    breakpoint($before);
                 } else {
                     $plugin = Build::plugin($object, $flags, $options, $record, str_replace('.', '_', $record['method']['name']));
                     $method_value = '$this->' . $plugin . '(';

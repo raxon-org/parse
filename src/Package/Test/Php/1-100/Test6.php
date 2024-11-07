71184a8b41b4ae0f21ae4ee1680a191a52e226f3
remco van der Velde <remco.pc@outlook.com>
2024-11-08 00:42:29 +0100

    init

--- src/Package/Test/Php/1-100/Test6.php
+++ src/Package/Test/Php/1-100/Test6.php
@@ -7,8 +7,9 @@ require '/Application/vendor/raxon/framework/src/Module/File.php';
 
 $dir = new \Raxon\Module\Dir();
 
-$read = $dir->read('/mnt/Vps3/Mount/Photo/Backup/Jan');
+$read = $dir->read('/mnt/Vps3/Mount/Photo/');
 foreach($read as $nr => $file) {
+    breakpoint($file);
     $file->new = $file->url . strtolower($file->name);
     \Raxon\Module\File::move($file->url, $file->new);
 }

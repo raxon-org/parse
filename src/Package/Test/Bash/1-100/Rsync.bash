5de73b1824eaaae48ad7f4daf8d05c4909f240a4
remco van der Velde <remco.pc@outlook.com>
2024-11-20 22:47:19 +0100

    ollama download model

--- src/Package/Test/Bash/1-100/Rsync.bash
+++ src/Package/Test/Bash/1-100/Rsync.bash
@@ -1,3 +1,3 @@
 rsync -avzP --progress /mnt/Vps2/Mount/Data/OpenAi/Embedding/ remco@10.0.0.10:/mnt/Disk2/Media/Software/Ai/Similarity/
 rsync -a --info=progress2 --info=name0 /mnt/Vps3/Mount/ remco@10.0.0.10:/mnt/Vps3/Mount/
-rsync -avzP --progress2 /mnt/Vps2/Mount/Data/RaXon/Oxford/Input/ remco@10.0.0.10:/mnt/Disk2/Media/Software/Oxford/Definition/Input/
+rsync -avzP --progress /mnt/Vps2/Mount/Data/RaXon/Oxford/Input/ remco@10.0.0.10:/mnt/Disk2/Media/Software/Oxford/Definition/Input/

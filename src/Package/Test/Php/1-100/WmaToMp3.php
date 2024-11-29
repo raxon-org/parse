4f69ba66fd3a973d5d99ae54594a3eb101738b5a
remco van der Velde <remco.pc@outlook.com>
2024-11-29 19:32:07 +0100

    ollama download model

--- src/Package/Test/Php/1-100/WmaToMp3.php
+++ src/Package/Test/Php/1-100/WmaToMp3.php
@@ -1,19 +1,61 @@
 <?php
-$app = new \Raxon\App();
+/**
+ * @author          Remco van der Velde
+ * @since           2020-10-27
+ * @version         1.0
+ * @changeLog
+ *     -    all
+ */
 
-ddd($app);
+use Raxon\App;
+use Raxon\Config;
 
-$dir = new \Raxon\Module\Dir();
+use Raxon\Exception\LocateException;
+use Raxon\Exception\ObjectException;
 
-$read = $dir->read('/mnt/Disk2/Media/Music/Cd/Shuffle/');
-foreach($read as $nr => $file) {
-    $explode = explode('.', $file->url);
-    $extension = array_pop($explode);
-    if(strtoupper($extension) === 'WMA'){
-        $file->new = implode('.', $explode) . '.mp3';
+$dir = __DIR__;
+$dir_vendor =
+    dirname($dir, 1) .
+    DIRECTORY_SEPARATOR .
+    'vendor' .
+    DIRECTORY_SEPARATOR;
+
+$autoload = $dir_vendor . 'autoload.php';
+$autoload = require $autoload;
+try {
+    $config = new Config(
+        [
+            'dir.vendor' => $dir_vendor,
+            'time.start' => microtime(true),
+        ]
+    );
+    $app = new App($autoload, $config);
+
+    $dir = new \Raxon\Module\Dir();
+
+    $read = $dir->read('/mnt/Disk2/Media/Music/Cd/Shuffle/');
+    foreach($read as $nr => $file) {
+        $explode = explode('.', $file->url);
+        $extension = array_pop($explode);
+        if(strtoupper($extension) === 'WMA'){
+            $file->new = implode('.', $explode) . '.mp3';
+        }
+        if(strtoupper($extension) === 'WAV'){
+            $file->new = implode('.', $explode) . '.mp3';
+        }
+        exec('ffmpeg -i ' . $file->url . ' -vn -ar 44100 -ac 2 -ab 320k -f mp3 ' . $file->new);
     }
-    if(strtoupper($extension) === 'WAV'){
-        $file->new = implode('.', $explode) . '.mp3';
+    /*
+    $result = App::run($app);
+    if(is_scalar($result)){
+        echo $result;
     }
-    exec('ffmpeg -i ' . $file->url . ' -vn -ar 44100 -ac 2 -ab 320k -f mp3 ' . $file->new);
+    elseif(is_array($result)) {
+        echo implode(PHP_EOL, $result);
+    }
+    */
+} catch (Exception | LocateException | ObjectException $exception) {
+    echo $exception;
 }
+
+

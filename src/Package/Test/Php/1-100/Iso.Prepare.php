c38f0299430805845008b356768e0269cfebbb5d
remco van der Velde <remco.pc@outlook.com>
2024-12-04 19:32:39 +0100

    Cli gotoxy (x, y) -> menu's

--- src/Package/Test/Php/1-100/Iso.Prepare.php
+++ src/Package/Test/Php/1-100/Iso.Prepare.php
@@ -65,9 +65,9 @@ try {
         $file->uuid = Core::uuid();
         $file->dir_number = $dir_number;
         $data->set('Tree.' . $nr, $file);
-        $data = gzencode(File::read($file->url), 9);
+        $read_gz= gzencode(File::read($file->url), 9);
         breakpoint($data);
-        File::write($target_dir . $dir_number . '/' . $file->uuid . $app->config('extension.gzip'), $data);
+        File::write($target_dir . $dir_number . '/' . $file->uuid . $app->config('extension.gzip'), $read_gz);
     }
     $size_format = File::size_format($size_total);
     $data->set('Summary.size', $size_total);

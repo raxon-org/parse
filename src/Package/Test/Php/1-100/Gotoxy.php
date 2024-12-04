4aba0d455de0a094785167bc8feda7e634e8aefe
remco van der Velde <remco.pc@outlook.com>
2024-12-04 06:11:54 +0100

    Cli gotoxy (x, y) -> menu's

---
+++ src/Package/Test/Php/1-100/Gotoxy.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * @author          Remco van der Velde
+ * @since           2020-10-27
+ * @version         1.0
+ * @changeLog
+ *     -    all
+ */
+
+use Raxon\App;
+use Raxon\Config;
+use Raxon\Module\Cli;
+
+use Raxon\Exception\LocateException;
+use Raxon\Exception\ObjectException;
+
+$dir = __DIR__;
+$dir_vendor =
+    DIRECTORY_SEPARATOR .
+    'Application' .
+    DIRECTORY_SEPARATOR .
+    'vendor' .
+    DIRECTORY_SEPARATOR;
+
+$autoload = $dir_vendor . 'autoload.php';
+$autoload = require $autoload;
+try {
+    $config = new Config(
+        [
+            'dir.vendor' => $dir_vendor,
+            'time.start' => microtime(true),
+        ]
+    );
+    $app = new App($autoload, $config);
+
+    Cli::gotoxy(10,10);
+    echo 'Hello World!';
+} catch (Exception | LocateException | ObjectException $exception) {
+    echo $exception;
+}
+
+

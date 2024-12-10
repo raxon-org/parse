6a1f6b54ac35413eeefe436c63ea2889db670fd2
remco van der Velde <remco.pc@outlook.com>
2024-12-10 17:57:25 +0100

    Cli gotoxy (x, y) -> menu's

--- src/Package/Test/Php/1-100/WmaToMp3.php
+++ src/Package/Test/Php/1-100/WmaToMp3.php
@@ -51,7 +51,31 @@ try {
             if(File::exist($file->new)){
                 continue;
             }
-            $command = 'ffmpeg -i \'' . str_replace('\'', '\\\'', $file->url) . '\' -vn -ar 44100 -ac 2 -ab 320k -f mp3 \'' . str_replace('\'', '\\\'', $file->new) . '\'';
+            $command = 'ffmpeg -i \'' . str_replace(
+                [
+                    '\'',
+                    '(',
+                    ')',
+                ],
+                [
+                    '\\\'',
+                    '\\(',
+                    '\\)',
+                ],
+                $file->url
+            ) . '\' -vn -ar 44100 -ac 2 -ab 320k -f mp3 \'' . str_replace(
+                [
+                    '\'',
+                    '(',
+                    ')',
+                ],
+                [
+                    '\\\'',
+                    '\\(',
+                    '\\)',
+                ],
+                $file->new
+            ) . '\'';
             echo $command . PHP_EOL;
             exec($command);
         }

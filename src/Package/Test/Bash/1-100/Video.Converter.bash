573db75de84c559de50a5a8c2fa7e57e461ab5f8
remco van der Velde <remco.pc@outlook.com>
2024-12-15 08:57:57 +0100

    Cli gotoxy (x, y) -> menu's

--- src/Package/Test/Bash/1-100/Video.Converter.bash
+++ src/Package/Test/Bash/1-100/Video.Converter.bash
@@ -1,1 +1,3 @@
 ffmpeg -i GOPR1260.MP4 -vf yadif -c:v libx264 -threads 4 -crf 18 -r 25 Ferrari.mp4 (#mp4 only no webm)
+
+ffmpeg -i VTS_01_1.VOB -vf yadif -c:v libvpx-vp9 -crf 18 -b:v 0 -threads 4 -r 25 -c:a libvorbis vts_o1_1.webm (#webm only no mp4)

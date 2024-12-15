c55fd8f280d7ca2fe38952d012f52b19d40cfc2e
remco van der Velde <remco.pc@outlook.com>
2024-12-15 15:15:06 +0100

    Batch movie dir  to Webm

--- src/Package/Test/Php/1-100/Batch.To.Webm.php
+++ src/Package/Test/Php/1-100/Batch.To.Webm.php
@@ -49,7 +49,7 @@ try {
                 continue;
             }
             if(strtolower($file->extension) === 'vob'){
-                $command = 'ffmpeg -i ' . $file->url . ' -vf yadif -c:v libvpx-vp9 -crf 18 -b:v 0 -threads 4 -r 25 -c:a libvorbis ' . $file->target;
+                $command = 'ffmpeg -i \'' . $file->url . '\' -vf yadif -c:v libvpx-vp9 -crf 18 -b:v 0 -threads 4 -r 25 -c:a libvorbis \'' . $file->target . '\'';
                 breakpoint($command);
                 Core::execute($app, $command, $output, $notification);
                 if($output){

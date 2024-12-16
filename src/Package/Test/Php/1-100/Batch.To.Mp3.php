a0d2d16d3cd4d88792d6fe3b167d9fd6952ef91d
remco van der Velde <remco.pc@outlook.com>
2024-12-16 19:17:42 +0100

    Batch movie dir  to Webm

--- src/Package/Test/Php/1-100/Batch.To.Mp3.php
+++ src/Package/Test/Php/1-100/Batch.To.Mp3.php
@@ -49,15 +49,15 @@ try {
             $file->new = implode('.', $explode) . '.mp3';
         }
         if($file->new !== false){
-            $file->new = str_replace(['"'], '\\"', $file->new);
+            $file->new = str_replace(['\'', '"'], '', $file->new);
             if(File::exist($file->new)){
                 continue;
             }
             File::move($file->url, $file->url . '.temp');
             $command = 'ffmpeg -i "' .
-                str_replace(['(', ')', '"'], ['(', ')', '\\"'], $file->url) . '.temp' .
+                str_replace(['(', ')', '"'], ['\\(', '\\)', '\\"'], $file->url) . '.temp' .
                 '" -vn -ar 44100 -ac 2 -ab 320k -f mp3 \'' .
-                str_replace(['(', ')', '"'], ['(', ')', '\\"'], $file->new) .
+                str_replace(['(', ')', '\''], ['\\(', '\\)', '\\\''], $file->new) .
                 '\'';
             echo $command . PHP_EOL;
             exec($command);

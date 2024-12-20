7c8a65b20ae64c2d708f8ae840acd01ba8cc0f5e
remco van der Velde <remco.pc@outlook.com>
2024-12-21 00:01:34 +0100

    bash init

---
+++ src/Plugin/App/App_Speak.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * @package Plugin\Modifier
+ * @author Remco van der Velde
+ * @since 2024-08-19
+ * @license MIT
+ * @version 1.0
+ * @changeLog
+ *    - all
+ */
+namespace Plugin;
+
+use Exception;
+
+use Raxon\App as Framework;
+
+use Raxon\Module\Core;
+use Raxon\Module\Dir;
+use Raxon\Module\File;
+
+trait App_Speak {
+
+    /**
+     * @throws Exception
+     */
+    protected function app_speak($speak='', $options=[])
+    {
+        $app = $this->object();
+        $options = Core::object($options, Core::ARRAY);
+        if(!array_key_exists('url', $options)){
+            $options['url'] = false;
+        }
+        if($options['url'] !== false){
+            $url = $options['url'];
+            $dir = Dir::name($url);
+            Dir::Create($dir, Dir::CHMOD);
+        } else {
+            $dir = '/mnt/Disk2/Media/Voice/';
+            $uuid = Core::uuid();
+            $url = $dir . $uuid . '.wav';
+        }
+        $command = 'espeak -v en -w ' . $url . ' "' . escapeshellarg($speak) . '"';
+        exec($command);
+        File::permission($app, [
+            'dir' => $dir,
+            'url' => $url
+        ]);
+    }
+}

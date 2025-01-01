3c701e4d97a6545cb2417aca3a253c645610ac1f
remco van der Velde <remco.pc@outlook.com>
2025-01-01 13:58:48 +0100

    tada

--- src/Package/Test/Bash/1-100/Video.Downloads.bash
+++ src/Package/Test/Bash/1-100/Video.Downloads.bash
@@ -1,2 +1,3 @@
  youtube-dl -v -x --restrict-filenames --prefer-ffmpeg https://www.youtube.com/watch?v=JGwWNGJdvx8 > /tmp/youtube-dl.log
  youtube-dl --list-formats
+ yt-dlp -f webm --restrict-filenames --prefer-ffmpeg https://www.youtube.com/watch?v=JGwWNGJdvx8  2>&1 >> /tmp/youtube-dl.log

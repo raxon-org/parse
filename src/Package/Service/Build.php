2b3ecaafe4720df5b2d5a7e97906cec0c47ed683
remco van der Velde <remco.pc@outlook.com>
2025-01-03 15:25:45 +0100

    init

--- src/Package/Service/Build.php
+++ src/Package/Service/Build.php
@@ -1453,6 +1453,7 @@ class Build
             if(!in_array($use_plugin, $use, true)){
                 $autoload = $object->data(App::AUTOLOAD_RAXON);
                 $location = $autoload->locate($use_plugin, false,  Autoload::MODE_LOCATION);
+                d($location);
                 $exist = false;
                 $locate_exception = [];
                 foreach($location  as $nr => $fileList){

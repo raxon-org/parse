e1bab3b1a67cbc06f464a6e8423ddafd4bf0c2de
remco van der Velde <remco.pc@outlook.com>
2025-01-19 17:50:17 +0100

    apache config generation

--- src/Package/Test/Php/1-100/Codepoint.Array.php
+++ src/Package/Test/Php/1-100/Codepoint.Array.php
@@ -1,6 +1,44 @@
 <?php
+/**
+ * @author          Remco van der Velde
+ * @since           2020-10-27
+ * @version         1.0
+ * @changeLog
+ *     -    all
+ */
 
-$list = [];
-for($i=0; $i<=0x10FFFF; $i++){
-    $list[$i] = mb_chr($i);
+use Raxon\App;
+use Raxon\Config;
+
+use Raxon\Exception\LocateException;
+use Raxon\Exception\ObjectException;
+
+$dir = __DIR__;
+$dir_vendor =
+    DIRECTORY_SEPARATOR .
+    'Application' .
+    DIRECTORY_SEPARATOR .
+    'vendor' .
+    DIRECTORY_SEPARATOR;
+
+$autoload = $dir_vendor . 'autoload.php';
+$autoload = require $autoload;
+try {
+    $config = new Config(
+        [
+            'dir.vendor' => $dir_vendor,
+            'time.start' => microtime(true),
+        ]
+    );
+    $app = new App($autoload, $config);
+
+    $list = [];
+    for($i=0; $i<=0x10FFFF; $i++){
+        $list[$i] = mb_chr($i);
+    }
+    d($list);
+} catch (Exception|LocateException|ObjectException $exception) {
+    echo $exception;
 }
+
+

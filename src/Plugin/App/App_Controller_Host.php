daf1b51b9d57105c8d39a127871610bfec45f210
remco van der Velde <remco.pc@outlook.com>
2025-02-11 20:26:23 +0100

    app can pass through simple objects

---
+++ src/Plugin/App/App_Controller_Host.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * @package Plugin\Modifier
+ * @author Remco van der Velde
+ * @since 2025-02-11
+ * @license MIT
+ * @version 1.0
+ * @changeLog
+ *    - all
+ */
+namespace Plugin;
+
+use Exception;
+use Raxon\App as Framework;
+
+trait App_Controller_Host {
+
+    /**
+     * @throws Exception
+     */
+    protected function app_controller_host(string $host): string
+    {
+        $explode = explode('.', $host);
+        foreach($explode as $nr => $value){
+            $explode[$nr] = ucfirst($value);
+        }
+        $host = implode('.', $explode);
+        return $host;
+
+    }
+}

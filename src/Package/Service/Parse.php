39f480f43f446bde2c8d34c7d81a42843ba1c810
remco van der Velde <remco.pc@outlook.com>
2025-02-12 00:52:54 +0100

    moving to new Parser raxon/parse

--- src/Package/Service/Parse.php
+++ src/Package/Service/Parse.php
@@ -36,7 +36,6 @@ class Parse
     public function __construct(App $object, Data $data, $flags=null, $options=null){
         $this->object($object);
         $this->data($data);
-        ddd($this->data());
         if($flags === null){
             $flags = (object) [];
         }

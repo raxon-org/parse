a945b0bd0eafb54b6261c1ef3eb631715f2cc578
remco van der Velde <remco.pc@outlook.com>
2025-02-12 01:05:51 +0100

    moving to new Parser raxon/parse

--- src/Package/Service/Build.php
+++ src/Package/Service/Build.php
@@ -1478,6 +1478,7 @@ class Build
             ){
                 if(!in_array($use_plugin, $use, true)){
                     //pre scanning for the right exception
+                    //this one breakpoint is wrong, it should not contain controller
                     breakpoint($object->config('controller.dir.plugin'));
                     $autoload = $object->data(App::AUTOLOAD_RAXON);
                     $location = $autoload->locate($use_plugin, false,  Autoload::MODE_LOCATION);

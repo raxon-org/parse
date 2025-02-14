3920008257d9131e4872489e946ff85059c74ffd
remco van der Velde <remco.pc@outlook.com>
2025-02-14 19:54:01 +0100

    p

---
+++ src/Plugin/Route/Route_Export.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * @package Plugin\Modifier
+ * @author Remco van der Velde
+ * @since 2024-08-19
+ * @license MIT
+ * @version 1.0
+ * @changeLog
+ *    - all
+ */
+
+use Raxon\App;
+
+use Raxon\Module\Route;
+
+trait Route_Export {
+
+    protected function route_export(): array
+    {
+        $object = $this->object();
+        $route = $object->data(App::ROUTE);
+        $list = $route->data();
+        $result = [];
+        foreach($list as $nr => $record){
+            $result[$nr] = Route::controller($record);
+        }
+        return $result;
+    }
+}

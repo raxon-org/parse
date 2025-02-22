e7dd62f15fb8f4db1a8734c6095c575ca18cf8f1
remco van der Velde <remco.pc@outlook.com>
2025-02-22 22:27:18 +0100

    moving parser closer to installation progress, because things aint cooking...

---
+++ src/Plugin/Plugin.Exit.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @package Plugin\Modifier
+ * @author Remco van der Velde
+ * @since 2024-08-19
+ * @license MIT
+ * @version 1.0
+ * @changeLog
+ *    - all
+ */
+namespace Plugin;
+
+trait Plugin_Exit {
+
+    protected function plugin_exit($status=0): void
+    {
+        exit($status);
+    }
+}

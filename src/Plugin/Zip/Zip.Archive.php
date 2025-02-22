cda5a8a14b0ec00fcb3ffab0a3c83f0ae62f5765
remco van der Velde <remco.pc@outlook.com>
2025-02-22 22:12:31 +0100

    moving parser closer to installation progress, because things aint cooking...

---
+++ src/Plugin/Zip/Zip.Archive.php
@@ -0,0 +1,131 @@
+<?php
+namespace PLugin;
+/**
+ * @package Plugin
+ * @author Remco van der Velde
+ * @since 2025-02-22
+ * @license MIT
+ * @version 1.0
+ * @changeLog
+ *    - all
+ */
+
+use Raxon\Module\File;
+use Raxon\Module\Dir;
+
+use Exception;
+
+use Raxon\Exception\DirectoryCreateException;
+use Raxon\Exception\FileWriteException;
+trait Zip_Archive {
+
+    /**
+     * @throws DirectoryCreateException
+     * @throws FileWriteException
+     * @throws Exception
+     */
+    public function zip_archive(object $flags, object $options): ?string
+    {
+        $object = $this->object();
+        $source = $options->source ?? null;
+        if(!$source){
+            throw new Exception('options->source is not set...');
+        }
+        $target = $options->target ?? null;
+        $parse = $this->parse();
+        /*
+        $limit = $parse->limit();
+        $parse->limit([
+            'plugin' => [
+                'date'
+            ]
+        ]);
+        */
+        $data = $this->data();
+        try {
+            $target = $parse->compile($target, $data);
+            //$parse->setLimit($limit);
+        } catch (Exception $exception) {
+            echo $exception->getMessage() . PHP_EOL;
+            return null;
+        }
+        if(
+            Dir::is($source) &&
+            !File::exist($target)
+        ){
+            $dir = new Dir();
+            $read = $dir->read($source, true);
+            $host = [];
+            if(!is_array($read)){
+                return null;
+            }
+            foreach($read as $file){
+                $host[] = $file;
+            }
+            $dir = Dir::name($target);
+            if(
+                $dir &&
+                !in_array(
+                    $dir,
+                    [
+                        $object->config('ds')
+                    ],
+                    true
+                )
+            ){
+                Dir::create($dir);
+            }
+            $zip = new \ZipArchive();
+            $zip->open($target, \ZipArchive::CREATE);
+            foreach($host as $file){
+                $location = false;
+                if(substr($file->url, 0, 1) === $object->config('ds')){
+                    $location = substr($file->url, 1);
+                } else {
+                    $location = $file->url;
+                }
+                if(!empty($location)){
+                    if($file->type === Dir::TYPE){
+                        $zip->addEmptyDir($location);
+                    } else {
+                        $zip->addFile($source, $location);
+                    }
+                }
+            }
+            $zip->close();
+            return $target;
+        }
+        elseif(
+            File::is($source) &&
+            !File::exist($target)
+        ) {
+            $dir = Dir::name($target);
+            if(
+                $dir &&
+                !in_array(
+                    $dir,
+                    [
+                        $object->config('ds')
+                    ],
+                    true
+                )
+            ){
+                Dir::create($dir);
+            }
+            $zip = new \ZipArchive();
+            $zip->open($target, \ZipArchive::CREATE);
+            $location = false;
+            if(substr($source, 0, 1) === $object->config('ds')){
+                $location = substr($source, 1);
+            } else {
+                $location = $source;
+            }
+            if(!empty($location)){
+                $zip->addFile($source, $location);
+            }
+            $zip->close();
+        }
+        return $target;
+    }
+
+}

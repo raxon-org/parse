5cf01c0fb6bb134f53dfd81e5e2d00bb9e40451f
remco van der Velde <remco.pc@outlook.com>
2025-02-23 09:46:17 +0100

    moving parser closer to installation progress, because things aint cooking...

---
+++ src/Plugin/File/File_Read.php
@@ -0,0 +1,50 @@
+<?php
+namespace Plugin;
+/**
+ * @package Plugin
+ * @author Remco van der Velde
+ * @since 2025-02-22
+ * @license MIT
+ * @version 1.0
+ * @changeLog
+ *    - all
+ */
+
+use Raxon\Module\File;
+
+trait File_Read {
+
+    public function file_read(string $url, array $options=[]): array | string
+    {
+        $object = $this->object();
+        ddd($object->config('require.url'));
+        if(File::exist($url)){
+            $mtime = File::mtime($url);
+            $require_disabled = $object->config('require.disabled');
+            if($require_disabled){
+                //nothing
+            } else {
+                $require_url = $object->config('require.url');
+                $require_mtime = $object->config('require.mtime');
+                if(empty($require_url)){
+                    $require_url = [];
+                    $require_mtime = [];
+                }
+                if(
+                    !in_array(
+                        $url,
+                        $require_url,
+                        true
+                    )
+                ){
+                    $require_url[] = $url;
+                    $require_mtime[] = $mtime;
+                    $object->config('require.url', $require_url);
+                    $object->config('require.mtime', $require_mtime);
+                }
+            }
+        }
+        return File::read($url, $options);
+    }
+
+}

950443d758f83210787054ca4f343f42e9506c67
remco van der Velde <remco.pc@outlook.com>
2025-02-24 21:23:19 +0100

    moving parser closer to installation progress, because things aint cooking...

---
+++ src/Plugin/Trace.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * @package Plugin\Modifier
+ * @author Remco van der Velde
+ * @since 2024-08-19
+ * @license MIT
+ * @version 1.0
+ * @changeLog
+ *    - all
+ */
+namespace Plugin;
+
+trait Trace {
+
+    public function trace($length=null): void
+    {
+        trace($length);
+    }
+
+}

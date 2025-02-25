46538b4ce61c44b47c94aabd2786a9e3748143e3
remco van der Velde <remco.pc@outlook.com>
2025-02-25 09:58:58 +0100

    moving parser closer to installation progress, because things aint cooking...

---
+++ src/Plugin/Script.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * @package Plugin\Modifier
+ * @author Remco van der Velde
+ * @since 2024-08-19
+ * @license MIT
+ * @version 1.0
+ * @changeLog
+ *    - all
+ */
+namespace Plugin;
+
+trait Script {
+
+    public function script(): void
+    {
+        $object = $this->object();
+        $args = func_get_args();
+        ddd($args);
+    }
+
+}

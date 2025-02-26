a7c375627cc0c374f9da7bfa1c016c5711c0e136
remco van der Velde <remco.pc@outlook.com>
2025-02-26 11:12:54 +0100

    1

--- src/Module/Parse.php
+++ src/Module/Parse.php
@@ -338,6 +338,7 @@ class Parse {
             $storage = $this->storage(new Data());
         }
         if(is_object($data)){
+            ddd($storage);
             $storage->data(Core::object_merge($storage->data(), $data));
         } else {
             $storage->data($data);

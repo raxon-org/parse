2969f22eaff12ed0163f1d442e0b2a7940821863
remco van der Velde <remco.pc@outlook.com>
2025-02-26 10:15:52 +0100

    1

--- src/Module/Token.php
+++ src/Module/Token.php
@@ -438,6 +438,7 @@ class Token {
      */
     public static function tree_prepare($string='', &$count=0, $options=[]): array
     {
+        d($string);
         $array = Token::split($string);
         $token = array();
         $row = 1;

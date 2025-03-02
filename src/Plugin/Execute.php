7c3c4a302556cff2f130495704a9ad6f6b0f3183
remco van der Velde <remco.pc@outlook.com>
2025-03-02 22:45:20 +0100

    make functions like new parser (traits)

---
+++ src/Plugin/Execute.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Plugin;
+
+use Exception;
+use Raxon\Module\Core;
+
+trait Execute
+{
+
+    /**
+     * @throws Exception
+     */
+    protected function execute(string $command='', $notification=''): ?string
+    {
+        $object = $this->object();
+        $data = $this->data();
+        $command = escapeshellcmd($command);
+        $output = false;
+        Core::execute($object, $command, $output, $notify);
+        if($notification){
+            if(
+                is_string($notification) &&
+                substr($notification, 0, 1) === '$'
+            ){
+                $notification = substr($notification, 1);
+            }
+            $data->data($notification, $notify);
+        }
+//    exec($command, $output);
+        return $output;
+    }
+}

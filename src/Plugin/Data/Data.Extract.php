a9cd8ca99a2c3d4e7a95e3f93fce55a074ac4218
remco van der Velde <remco.pc@outlook.com>
2025-03-03 19:58:11 +0100

    make functions like new parser (traits)

---
+++ src/Plugin/Data/Data.Extract.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * @package Plugin\Modifier
+ * @author Remco van der Velde
+ * @since 2024-08-19
+ * @license MIT
+ * @version 1.0
+ * @changeLog
+ *    - all
+ */
+namespace Plugin;
+
+use Exception;
+
+trait Data_Extract {
+
+    /**
+     * @throws Exception
+     */
+    protected function data_extract(string $attribute=''): mixed
+    {
+        if(
+            is_string($attribute) &&
+            substr($attribute, 0, 1) === '$'
+        ){
+            $attribute = substr($attribute, 1);
+        }
+        $data = $this->data();
+        return $data->extract($attribute);
+    }
+}

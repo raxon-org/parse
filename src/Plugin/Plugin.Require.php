e842a2e9517903dc94726b2f294d07f025f8bf58
remco van der Velde <remco.pc@outlook.com>
2025-03-03 11:47:32 +0100

    make functions like new parser (traits)

--- src/Plugin/Plugin.Require.php
+++ src/Plugin/Plugin.Require.php
@@ -10,12 +10,13 @@
  */
 namespace Plugin;
 
-use Raxon\App;
+use Raxon\Config;
+
+use Raxon\Module\Autoload;
 use Raxon\Module\Data;
 use Raxon\Module\Dir;
 use Raxon\Module\File;
-
-use RaXon\Parse\Module\Parse;
+use Raxon\Module\Parse;
 
 use Exception;
 
@@ -24,56 +25,142 @@ trait Plugin_require {
     /**
      * @throws Exception
      */
-    protected function plugin_require(string $url, mixed $data=null): void
+    protected function plugin_require(string $url, mixed $data=null): string
     {
         $object = $this->object();
-        d($url);
-        ddd($data);
-
-        $dir = Dir::name($object->config('package.raxon/parse.build.state.source.url'));
-        if(substr($url, 0, 2) === './'){
-            $url = $dir . substr($url, 2);
-        }
-        elseif(str_contains($url, '/') === false){
-            $url = $dir . $url;
-        }
+        $storage = $this->data();
+        $cache_url = false;
+        $cache_dir = false;
+        $is_cache_url = false;
         if(!File::exist($url)) {
-            $text = 'Require: file not found: ' . $url . ' in template: ' . $object->config('package.raxon/parse.build.state.source.url');
+            $text = 'Require: file not found: ' . $url . ' in template: ' . $storage->data('raxon.org.parse.view.source.url');
             throw new Exception($text);
         }
-        $data = $this->plugin_require_data($data);
-        $flags = App::flags($object);
-        $options = App::options($object);
-        $options_require = clone $options;
-        $options_require->source = $url;
-        unset($options_require->hash);
-        unset($options_require->class);
-        unset($options_require->namespace);
-        $parse = new Parse($object, $data, $flags, $options_require);
-        $read = File::read($url);
-        echo $parse->compile($read);
-    }
-
-    /**
-     * @throws Exception
-     */
-    protected function plugin_require_data(mixed $data=null): ?Data
-    {
-        if(is_array($data)){
-            $data = new Data($data);
+        $mtime = File::mtime($url);
+        if($object->config('ramdisk.url')){
+            $is_plugin = false;
+            $plugin_list = $object->config('cache.parse.plugin.list');
+            foreach($plugin_list as $plugin){
+                if(
+                    property_exists($plugin, 'name') &&
+                    $plugin->name === 'require'
+                ){
+                    $is_plugin = $plugin;
+                    break;
+                }
+            }
+            if(
+                $is_plugin &&
+                property_exists($is_plugin, 'name_length') &&
+                property_exists($is_plugin, 'name_separator') &&
+                property_exists($is_plugin, 'name_pop_or_shift')
+            ){
+                $cache_url = $object->config('ramdisk.url') .
+                    $object->config(Config::POSIX_ID) .
+                    $object->config('ds') .
+                    $object->config('dictionary.view') .
+                    $object->config('ds') .
+                    Autoload::name_reducer(
+                        $object,
+                        str_replace('/', '_', $url),
+                        $is_plugin->name_length,
+                        $is_plugin->name_separator,
+                        $is_plugin->name_pop_or_shift
+                    );
+                ;
+                $cache_dir = Dir::name($cache_url);
+                if(
+                    File::exist($cache_url) &&
+                    File::mtime($cache_url) === $mtime
+                ){
+                    $url = $cache_url;
+                    $is_cache_url = true;
+                }
+            }
         }
-        elseif(
-            is_object($data) &&
-            $data instanceof Data
-        ){
+        $require_disabled = $object->config('require.disabled');
+        if($require_disabled){
             //nothing
-        }
-        elseif(is_object($data)) {
-            $data = new Data($data);
         } else {
-            $data = new Data();
+            $require_url = $object->config('require.url');
+            $require_mtime = $object->config('require.mtime');
+            if(empty($require_url)){
+                $require_url = [];
+                $require_mtime = [];
+            }
+            if(
+                !in_array(
+                    $url,
+                    $require_url,
+                    true
+                )
+            ){
+                $require_url[] = $url;
+                $require_mtime[] = $mtime;
+                $object->config('require.url', $require_url);
+                $object->config('require.mtime', $require_mtime);
+            }
         }
-        return $data;
+        $read = File::read($url);
+        if(
+            $is_cache_url === false &&
+            $object->config('ramdisk.url') &&
+            $cache_dir !== false &&
+            $cache_url !== false
+        ){
+            //copy to ramdisk
+            Dir::create($cache_dir);
+            File::copy($url, $cache_url);
+            File::touch($cache_url, File::mtime($url));
+            exec('chmod 640 ' . $cache_url);
+        }
+        if(!empty($data)){
+            $data_data = new Data();
+            $data_data->data($data);
+            $data_data->data('raxon.org.parse.view.source.url', $url);
+            $data_data->data('ldelim', '{');
+            $data_data->data('rdelim', '}');
+            $storage->data('raxon.org.parse.view.source.mtime', $mtime);
+//        ob_start();
+            $parser = new Parse($object);
+            $compile =  $parser->compile($read, [], $data_data);
+//        d($compile);
+            $data_script = $data_data->data('script');
+            $script = $storage->data('script');
+            if(!empty($data_script) && empty($script)){
+                $storage->data('script', $data_script);
+            }
+            elseif(!empty($data_script && !empty($script))){
+                foreach($script as $nr => $value){
+                    if(in_array($value, $data_script, true)){
+                        unset($script[$nr]);
+                    }
+                }
+                $storage->data('script', array_merge($script, $data_script));
+            }
+            $data_link = $data_data->data('link');
+            $link = $storage->data('link');
+            if(!empty($data_link) && empty($link)){
+                $storage->data('link', $data_link);
+            }
+            elseif(!empty($data_link && !empty($link))){
+                foreach($link as $nr => $value){
+                    if(in_array($value, $data_link, true)){
+                        unset($link[$nr]);
+                    }
+                }
+                $storage->data('link', array_merge($link, $data_link));
+            }
+            return $compile;
+        } else {
+            $source = $storage->data('raxon.org.parse.view.source.url');
+            $storage->data('raxon.org.parse.view.source.url', $url);
+            $storage->data('raxon.org.parse.view.source.mtime', $mtime);
+            $parser = new Parse($object);
+            $compile = $parser->compile($read, [], $storage);
+            $storage->data('raxon.org.parse.view.source.url', $source);
+            return $compile;
+        }
     }
 
 }

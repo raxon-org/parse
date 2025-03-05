ebd88f180b0a1b4e285f38c987e1c3af0c0162c8
remco van der Velde <remco.pc@outlook.com>
2025-03-05 13:34:11 +0100

    make modifier like method

--- src/Plugin/Json/Json.Encode.php
+++ src/Plugin/Json/Json.Encode.php
@@ -1,13 +1,5 @@
 <?php
-/**
- * @package Plugin
- * @author Remco van der Velde
- * @since 2024-08-19
- * @license MIT
- * @version 1.0
- * @changeLog
- *    - all
- */
+namespace Plugin;
 
 
 trait Json_Encode {

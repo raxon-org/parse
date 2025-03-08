467666889422b97407e07b5883ddf527c2fd9749
remco van der Velde <remco.pc@outlook.com>
2025-03-08 07:58:36 +0100

    init

---
+++ src/Plugin/Block/Block.Html.php
@@ -0,0 +1,45 @@
+<?php
+
+use Exception;
+
+trait Block_Html {
+
+    protected function block_html(string $name='', $value=null): string
+    {
+        $data = $this->storage();
+        if($value === null){
+            $value = $name;
+            $name = null;
+        }
+        $search = [" ", "\t", "\n", "\r", "\r\n"];
+        $replace = ['','','','',''];
+        $content_html = trim($value);
+        $content_html = explode('<', $content_html);
+        foreach ($content_html as $nr => $row){
+            $dataRow = explode('>', $row);
+            if(count($dataRow)>=2){
+                foreach ($dataRow as $dataRowNr => $dataR){
+                    if($dataRowNr > 0){
+                        $tmp = str_replace($search, $replace, $dataR);
+                    } else {
+                        $tmp = $dataR;
+                    }
+                    if(empty($tmp)){
+                        $dataRow[$dataRowNr] = '';
+                    }
+                }
+                $content[$nr] = implode('>', $dataRow);
+            }
+        }
+        $value = implode('<', $content_html);
+        if(empty($name)){
+            $content = $data->data('#content');
+            $content[] = $value;
+            $data->data('#content', $content);
+            return $value;
+        } else {
+            $data->data($name, $value);
+        }
+        return '';
+    }
+}
--- src/Plugin/Html/Html.Target.Create.php
+++ src/Plugin/Html/Html.Target.Create.php
@@ -1,13 +1,8 @@
 <?php
 namespace Plugin;
 
-use Exception;
-
 trait Html_Target_Create {
 
-    /**
-     * @throws Exception
-     */
     protected function html_target_create(string $target=null, $options=[]): string
     {
         $result = $target;

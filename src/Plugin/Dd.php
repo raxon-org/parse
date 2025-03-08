38a40dde5f93f4d578e83584f0d81496ef90edac
remco van der Velde <remco.pc@outlook.com>
2025-03-08 06:23:34 +0100

    init

--- src/Plugin/Dd.php
+++ src/Plugin/Dd.php
@@ -38,6 +38,7 @@ trait Dd {
                 $value,
                 [
                     '$this',
+                    '{$this}',
                     '{{$this}}'
                 ],
                 true

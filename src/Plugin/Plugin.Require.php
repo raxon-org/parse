79c3ec3b5ac5f3e8ff6893086c39f15135e91ca0
remco van der Velde <remco.pc@outlook.com>
2025-03-08 05:02:37 +0100

    init

--- src/Plugin/Plugin.Require.php
+++ src/Plugin/Plugin.Require.php
@@ -118,9 +118,9 @@ trait Plugin_require {
             $data_data = new Data();
             $data_data->data($data);
             $data_data->data('raxon.org.parse.view.source.url', $url);
+            $data_data->data('raxon.org.parse.view.source.mtime', $mtime);
             $data_data->data('ldelim', '{');
             $data_data->data('rdelim', '}');
-            $storage->data('raxon.org.parse.view.source.mtime', $mtime);
 //        ob_start();
             $parser = new Parse($object);
             $compile =  $parser->compile($read, [], $data_data);
@@ -153,11 +153,38 @@ trait Plugin_require {
             }
             return $compile;
         } else {
+            $data_data = clone $storage;
             $source = $storage->data('raxon.org.parse.view.source.url');
-            $storage->data('raxon.org.parse.view.source.url', $url);
-            $storage->data('raxon.org.parse.view.source.mtime', $mtime);
+            $data_data->data('raxon.org.parse.view.source.url', $url);
+            $data_data->data('raxon.org.parse.view.source.mtime', $mtime);
             $parser = new Parse($object);
-            $compile = $parser->compile($read, [], $storage);
+            $compile = $parser->compile($read, [], $data_data);
+            $data_script = $data_data->data('script');
+            $script = $storage->data('script');
+            if(!empty($data_script) && empty($script)){
+                $storage->data('script', $data_script);
+            }
+            elseif(!empty($data_script && !empty($script))){
+                foreach($script as $nr => $value){
+                    if(in_array($value, $data_script, true)){
+                        unset($script[$nr]);
+                    }
+                }
+                $storage->data('script', array_merge($script, $data_script));
+            }
+            $data_link = $data_data->data('link');
+            $link = $storage->data('link');
+            if(!empty($data_link) && empty($link)){
+                $storage->data('link', $data_link);
+            }
+            elseif(!empty($data_link && !empty($link))){
+                foreach($link as $nr => $value){
+                    if(in_array($value, $data_link, true)){
+                        unset($link[$nr]);
+                    }
+                }
+                $storage->data('link', array_merge($link, $data_link));
+            }
             $storage->data('raxon.org.parse.view.source.url', $source);
             d($storage->data('script'));
             return $compile;

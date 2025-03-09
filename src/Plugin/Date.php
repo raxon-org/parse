6b076b627c838aa451b107beb42a94e4896e0211
remco van der Velde <remco.pc@outlook.com>
2025-03-09 22:01:18 +0100

    init

---
+++ src/Plugin/Date.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * @package Plugin\Modifier
+ * @author Remco van der Velde
+ * @since 2024-08-19
+ * @license MIT
+ * @version 1.0
+ * @changeLog
+ *    - all
+ */
+
+namespace Plugin;
+
+use Exception;
+use Raxon\Module\Core;
+
+trait Date
+{
+
+    /**
+     * @throws Exception
+     */
+    protected function date(string $format, $timestamp=null): ?string
+    {
+        if(empty($format)){
+            $format = 'Y-m-d H:i:s';
+        }
+        elseif($format === true){
+            $format = 'Y-m-d H:i:s P';
+        }
+        elseif(defined($format)){
+            $format = constant($format);
+        }
+        if($timestamp === null){
+            $timestamp = time();
+        }
+        return date($format, $timestamp);
+    }
+}

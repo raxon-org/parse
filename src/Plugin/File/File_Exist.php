ca10fbbd550adc0dcb89b32c7ffdd3d6e2bc601b
remco van der Velde <remco.pc@outlook.com>
2025-03-21 09:48:41 +0100

    sqlite

---
+++ src/Plugin/File/File_Exist.php
@@ -0,0 +1,22 @@
+<?php
+namespace Plugin;
+/**
+ * @package Plugin
+ * @author Remco van der Velde
+ * @since 2025-02-22
+ * @license MIT
+ * @version 1.0
+ * @changeLog
+ *    - all
+ */
+
+use Raxon\Module\File;
+
+trait File_Exist {
+
+    public function file_exist(string $url): bool
+    {
+        return File::exist($url);
+    }
+
+}

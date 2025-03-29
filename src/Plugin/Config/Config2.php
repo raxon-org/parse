bf6bcc0ea1e518f67286348c864ff994f55ce8bd
remco van der Velde <remco.pc@outlook.com>
2025-03-29 21:22:01 +0100

    ö

---
+++ src/Plugin/Config/Config2.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * @package Plugin\Modifier
+ * @author Remco van der Velde
+ * @since 2024-08-19
+ * @license MIT
+ * @version 1.0
+ * @changeLog
+ *    - all
+ */
+namespace Plugin;
+
+use Raxon\Parse\Attribute\Validate;
+
+trait Config2 {
+
+    #[Validate(
+        argument: [
+            "is.string && (length:1,255 || contains:['delete','remove'] || is.email || is.uuid)",
+            "is.string and (length:1,255 or contains:['delete','remove'] or is.email or is.uuid)",
+            "mixed",
+        ],
+        result: "mixed"
+    )]
+    protected function config2(string $attribute, mixed $value=null): mixed
+    {
+        $object = $this->object();
+//        breakpoint($object->config());
+        if($value !== null){
+            if(
+                in_array(
+                    $attribute,
+                    [
+                        'delete',
+                        'remove'
+                    ],
+                    true
+                )
+            ){
+                $object->config($attribute, $value);
+                return true;
+            }
+            return $object->config($attribute, $value);
+        }
+        return $object->config($attribute);
+    }
+}

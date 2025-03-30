5211d2276ec2a75c1a01516701d5102d01ad0437
remco van der Velde <remco.pc@outlook.com>
2025-03-30 21:09:55 +0200

    ö

--- src/Build/Php.php
+++ src/Build/Php.php
@@ -1034,7 +1034,7 @@ class Php {
                 $record['type'] === 'variable'
             ){
                 $uuid_variable = Core::uuid_variable();
-                $before[] = $uuid_variable . ' = $this->data(\'' . $record['name'] . '\');';
+                $before[] = $uuid_variable . ' = $data(\'' . $record['name'] . '\');';
                 $value .= $uuid_variable;
             }
             elseif(

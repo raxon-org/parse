4fb1b31547e557aba2380c90f976182ca394391f
remco van der Velde <remco.pc@outlook.com>
2025-04-01 19:03:09 +0200

    ö

--- src/Build/Php.php
+++ src/Build/Php.php
@@ -479,7 +479,14 @@ class Php {
                             }
                             $before = [];
                         }
-                        $data[] = $method . ';';
+                        $uuid_variable = Core::uuid_variable();
+                        $data[] = $uuid_variable . ' = ' . $method . ';';
+                        $data[] = 'if(is_scalar('. $uuid_variable . '){';
+                        $data[] = '    $content[] = ' .  $uuid_variable;
+                        $data[] = '} elseif(is_array('. $uuid_variable .') || is_object(' .  $uuid_variable . ') {';
+                        $data[] = '    return ' .  $uuid_variable  . ';';
+                        $data[] = '} else {'; //null
+                        $data[] = '}';
                         if(!empty($after)){
                             foreach($after as $line){
                                 $data[] = $line;

a5de56a87bdb6f2d00edeeffd70fcaed386d936a
remco van der Velde <remco.pc@outlook.com>
2025-04-06 13:47:42 +0200

    p

--- src/Build/Php.php
+++ src/Build/Php.php
@@ -2336,7 +2336,9 @@ class Php {
                 $data = [];
                 $before = [];
                 $after = [];
+                d($record);
                 $array_notation = Php::value($object, $flags, $options, $record, $record['variable']['array_notation'], $is_set, $before, $after);
+                d($array_notation);
                 $variable_value = '$data->get(\'' . $record['variable']['name'] . '\')' .  $array_notation;
                 foreach($before as $line){
                     $data[] = $line;

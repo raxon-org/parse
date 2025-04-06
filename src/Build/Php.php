b33dfeb68288bf07cdf3bb62ac643c4f77d2ba30
remco van der Velde <remco.pc@outlook.com>
2025-04-06 15:22:49 +0200

    p

--- src/Build/Php.php
+++ src/Build/Php.php
@@ -467,7 +467,6 @@ class Php {
                             $if_data[] = '}';
                             if(array_key_exists('elseif', $content)){
                                 foreach($content['elseif'] as $elseif_nr => $elseif){
-                                    d($elseif);
                                     if(array_key_exists('statement', $elseif)){
                                         $if_data[] = Php::method($object, $flags, $options, $elseif['statement'], $before, $after) . '{';
                                         if(!empty($before)){
@@ -484,6 +483,8 @@ class Php {
                                         }
                                         $object->config('package.raxon/parse.build.state.remove_newline_next', true);
                                         $if_content = Php::document_tag($object, $flags, $options, $elseif['content']);
+                                        d($elseif['content']);
+                                        d($if_content);
                                         foreach($if_content as $line){
                                             $if_data[] = $line;
                                         }

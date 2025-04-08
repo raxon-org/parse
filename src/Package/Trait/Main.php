fe25a05fdb09bdadd0988eb9b7c284ef34b1dd39
remco van der Velde <remco.pc@outlook.com>
2025-04-08 14:50:46 +0200

    compiling json

--- src/Package/Trait/Main.php
+++ src/Package/Trait/Main.php
@@ -21,12 +21,20 @@ trait Main {
         if(File::exist($options->source) === false){
             throw new Exception('Source not found: ' . $options->source);
         }
+        if(!property_exists($options, 'type')){
+            $options->type = 'string';
+        }
         $object = $this->object();
-        $extension = File::extension($options->source);
-        ddd($extension);
-        $input = File::read($options->source);
-        $parse = new Parse($object, new Data(), $flags, $options);
-        $object->config(Parse::CONFIG . '.build.builder', 'Compile'); //can be removed if bold is replaced.
-        return $parse->compile($input);
+        switch($options->type){
+            case 'data':
+            case 'json':
+            case 'jsonl':
+                return $object->parse_data($options->source);
+            default:
+                $input = File::read($options->source);
+                $parse = new Parse($object, new Data(), $flags, $options);
+                $object->config(Parse::CONFIG . '.build.builder', 'Compile'); //can be removed if bold is replaced.
+                return $parse->compile($input);
+        }
     }
 }

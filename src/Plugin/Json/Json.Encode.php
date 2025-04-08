4b7b5e9f7e9a529f9db7f60eaacfd4e9bd64daa2
remco van der Velde <remco.pc@outlook.com>
2025-04-08 14:55:03 +0200

    compiling json

--- src/Plugin/Json/Json.Encode.php
+++ src/Plugin/Json/Json.Encode.php
@@ -4,7 +4,7 @@ namespace Plugin;
 
 trait Json_Encode {
 
-    protected function json_encode(mixed $value, mixed $flags, int $depth=512): false | string
+    protected function json_encode(mixed $value, mixed $flags=0, int $depth=512): false | string
     {
         if(is_string($flags)){
             $flags = constant($flags);

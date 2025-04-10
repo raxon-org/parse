d2f26795bdef0fdb34628bba659036bc87891555
remco van der Velde <remco.pc@outlook.com>
2025-04-10 16:27:39 +0200

    test 007

--- src/Plugin/Terminal/Terminal_Readline.php
+++ src/Plugin/Terminal/Terminal_Readline.php
@@ -10,13 +10,16 @@ trait Terminal_Readline {
     /**
      * @throws ObjectException
      */
-    protected function terminal_readline(string $text, string $type=Cli::INPUT): string
+    protected function terminal_readline(string $text, string $type=null): string
     {
         if(
             $text === Cli::STREAM &&
             $type === null
         ){
             return Cli::read($text);
+        }
+        if($type === null){
+            $type = Cli::INPUT;
         }
         return Cli::read($type, $text);
     }

5412f391555a48f5a45d3283fbbc288baa040f7d
remco van der Velde <remco.pc@outlook.com>
2025-04-12 12:11:19 +0200

    test 001 user login json

--- src/Package/Test/Php/1-100/015.Undefined.Constant.Assign3.php
+++ src/Package/Test/Php/1-100/015.Undefined.Constant.Assign3.php
@@ -21,7 +21,7 @@ class Temp {
             $eelmggac_bolk_knif_pfpm_nacddngdmfob = '__METHOD__';
             $lgeagbfe_anmg_khhl_bgmf_gmceogidcnhh = __CLASS__2;
         } catch(Error | ErrorException | Exception | ParseError $exception) {
-            throw $exception;
+            echo $exception;
         }
     }
 }

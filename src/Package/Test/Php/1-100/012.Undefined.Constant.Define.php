16e50b05c32dde51c89baf22a6d229f6a0104d90
remco van der Velde <remco.pc@outlook.com>
2025-04-12 11:48:05 +0200

    test 001 user login json

---
+++ src/Package/Test/Php/1-100/012.Undefined.Constant.Define.php
@@ -0,0 +1,27 @@
+<?php
+set_error_handler(function ($severity, $message, $file, $line) {
+    throw new ErrorException($message, 0, $severity, $file, $line);
+});
+
+class Temp {
+    public function __construct()
+    {
+        echo 'Temp class instantiated' . PHP_EOL;
+    }
+
+    public function run()
+    {
+        try {
+            echo UNDEFINED_CONSTANT;
+        } catch (ErrorException|Exception $e) {
+            echo "Caught warning: " . $e->getMessage();
+        }
+    }
+
+}
+
+$obj = new Temp();
+$obj->run();
+
+
+

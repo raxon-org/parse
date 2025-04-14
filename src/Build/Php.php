9c7a0cae0bab8eb9c1f66c78dabc8b4e409eadef
remco van der Velde <remco.pc@outlook.com>
2025-04-14 08:00:54 +0200

    test 001 user login json

--- src/Build/Php.php
+++ src/Build/Php.php
@@ -2862,6 +2862,7 @@ class Php {
                 ){
                     $variable_old = $options->variable ?? null;
                     $options->variable = Core::uuid_variable();
+                    $before[] = $options->variable . ' = [];';
                     $token = Token::tokenize($object, $flags, $options, $record['execute']);
                     $token = Php::document_tag_prepare($object, $flags, $options, $token);
                     $embed = Php::document_tag($object, $flags, $options, $token);

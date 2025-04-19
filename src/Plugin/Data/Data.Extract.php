dfc5b769d96c878eeda457ca8d6d8b1156e44fbd
remco van der Velde <remco.pc@outlook.com>
2025-04-19 11:11:13 +0200

    array & file plugins

--- src/Plugin/Data/Data.Extract.php
+++ src/Plugin/Data/Data.Extract.php
@@ -11,14 +11,17 @@
 namespace Plugin;
 
 use Exception;
+use Raxon\Parse\Attribute\Argument;
 
 trait Data_Extract {
 
     /**
      * @throws Exception
      */
+    #[Argument(apply: "literal", count: 1)]
     protected function data_extract(string $attribute=''): mixed
     {
+        $attribute = trim($attribute, '\'"');
         if(
             is_string($attribute) &&
             substr($attribute, 0, 1) === '$'
---
+++ src/Plugin/Data/Data.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * @package Plugin\Modifier
+ * @author Remco van der Velde
+ * @since 2024-08-19
+ * @license MIT
+ * @version 1.0
+ * @changeLog
+ *    - all
+ */
+namespace Plugin;
+
+use Raxon\Module\Filter;
+
+use Exception;
+
+trait Data {
+
+    /**
+     * @throws Exception
+     */
+    protected function data(string $attribute=null, mixed $value=null): mixed
+    {
+        $data = $this->Data();
+        if(
+            $attribute === null &&
+            $value === null
+        ){
+            return $data->data();
+        }
+        elseif(is_string($attribute)) {
+            $attribute = trim($attribute, '\'"');
+            if(
+                substr($attribute, 0, 1) === '$'
+            ){
+                $attribute = substr($attribute, 1);
+            }
+            return $data->data($attribute, $value);
+        }
+        elseif($value === null){
+            return $data->data($attribute);
+        }
+        return null;
+    }
+}

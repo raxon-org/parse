db659de8803433a9287f985fcd651291c221e7ca
remco van der Velde <remco.pc@outlook.com>
2025-04-20 23:37:14 +0200

    preg plugins

--- src/Plugin/String/String.Replace.php
+++ src/Plugin/String/String.Replace.php
@@ -8,10 +8,6 @@ trait String_Replace {
     #[Argument(apply: "literal", count: 1, index:3)]
     protected function string_replace(array|string $subject='', array|string $search='', array|string $replace='', string $attribute=null): array|string
     {
-        d($search);
-        d($replace);
-        d($subject);
-        d($attribute);
         if(!empty($attribute)){
             $attribute = trim($attribute, '\'"');
             if(substr($attribute, 0, 1) == '$'){

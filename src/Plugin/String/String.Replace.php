bd8e2f1e614c67173fed8fda6903155be02fbcb4
remco van der Velde <remco.pc@outlook.com>
2025-04-20 23:28:56 +0200

    preg plugins

--- src/Plugin/String/String.Replace.php
+++ src/Plugin/String/String.Replace.php
@@ -8,6 +8,10 @@ trait String_Replace {
     #[Argument(apply: "literal", count: 1, index:3)]
     protected function string_replace(array|string $search='', array|string $replace='', array|string $subject='', string $attribute=null): array|string
     {
+        d($search);
+        d($replace);
+        d($subject);
+        d($attribute);
         if(!empty($attribute)){
             $attribute = trim($attribute, '\'"');
             if(substr($attribute, 0, 1) == '$'){

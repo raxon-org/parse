87ba06c266378fc6523ac35eeecef079a0c8b65d
remco van der Velde <remco.pc@outlook.com>
2025-04-22 17:46:44 +0200

    implementing new parser

--- src/Plugin/Parse/Parse.Read.php
+++ src/Plugin/Parse/Parse.Read.php
@@ -27,7 +27,6 @@ trait Parse_Read {
         if(File::exist($url)){
             $object = $this->object();
             d($url);
-            ddd($cache);
             if($cache){
                 $read = $object->compile_read($url, sha1($url));
             } else {

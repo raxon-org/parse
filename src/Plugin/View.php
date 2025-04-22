16f6646fc18795cde2a9fc77aafea0a0aadd16c3
remco van der Velde <remco.pc@outlook.com>
2025-04-22 23:47:02 +0200

    implementing new parser

--- src/Plugin/View.php
+++ src/Plugin/View.php
@@ -33,58 +33,52 @@ trait View {
         $mtime = File::mtime($url);
         $parse = $this->parse();
         $storage = $this->storage();
-        if(empty($data)){
-            $storage->data('raxon.org.parse.view.source.url', $url);
-            $storage->data('raxon.org.parse.view.source.mtime', $mtime);
-            $options = $parse->parse_options();
-            $view_options = (object) [];
-            $view_options->source = $url;
-            $parse->parse_options($view_options);
-            $read = $parse->compile($read, []);
-            $parse->parse_options($options);
+
+        $storage->data('raxon.org.parse.view.source.url', $url);
+        $storage->data('raxon.org.parse.view.source.mtime', $mtime);
+
+        if($data === null){
+            $data = new Data();
+        }
+        elseif(
+            is_object($data) &&
+            get_class($data) === 'Data'
+        ){
+            //nothing
         } else {
-            $storage->data('raxon.org.parse.view.source.url', $url);
-            $storage->data('raxon.org.parse.view.source.mtime', $mtime);
-            if(
-                is_object($data) &&
-                get_class($data) === 'Data'
-            ){
-                $data_data = $data;
-            } else {
-                $data_data = new Data($data);
-            }
-            $options = $parse->parse_options();
-            $view_options = (object) [];
-            $view_options->source = $url;
-            $parse->parse_options($view_options);
-            $read = $parse->compile($read, $data_data);
-            $parse->parse_options($options);
-            $data_script = $data_data->data('script');
-            $script = $data->data('script');
-            if(!empty($data_script) && empty($script)){
-                $data->data('script', $data_script);
-            }
-            elseif(!empty($data_script && !empty($script))){
-                foreach($script as $nr => $value){
-                    if(in_array($value, $data_script, true)){
-                        unset($script[$nr]);
-                    }
+            $data = new Data($data);
+        }
+        $options = $parse->parse_options();
+        $view_options = (object) [];
+        $view_options->source = $url;
+        $parse->parse_options($view_options);
+        $read = $parse->compile($read, $data);
+        $parse->parse_options($options);
+        $data_script = $data->data('script');
+        $script = $data->data('script');
+        if(!empty($data_script) && empty($script)){
+            $storage->data('script', $data_script);
+        }
+        elseif(!empty($data_script && !empty($script))){
+            foreach($script as $nr => $value){
+                if(in_array($value, $data_script, true)){
+                    unset($script[$nr]);
                 }
-                $data->data('script', array_merge($script, $data_script));
             }
-            $data_link = $data_data->data('link');
-            $link = $data->data('link');
-            if(!empty($data_link) && empty($link)){
-                $data->data('link', $data_link);
-            }
-            elseif(!empty($data_link && !empty($link))){
-                foreach($link as $nr => $value){
-                    if(in_array($value, $data_link, true)){
-                        unset($link[$nr]);
-                    }
+            $storage->data('script', array_merge($script, $data_script));
+        }
+        $data_link = $data->data('link');
+        $link = $data->data('link');
+        if(!empty($data_link) && empty($link)){
+            $storage->data('link', $data_link);
+        }
+        elseif(!empty($data_link && !empty($link))){
+            foreach($link as $nr => $value){
+                if(in_array($value, $data_link, true)){
+                    unset($link[$nr]);
                 }
-                $data->data('link', array_merge($link, $data_link));
             }
+            $storage->data('link', array_merge($link, $data_link));
         }
         return $read;
     }

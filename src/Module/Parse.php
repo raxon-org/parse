58de3ca72cba12e2e58315fcd1e924804af471c5
remco van der Velde <remco.pc@outlook.com>
2025-04-23 03:41:13 +0200

    implementing new parser

--- src/Module/Parse.php
+++ src/Module/Parse.php
@@ -456,7 +456,7 @@ class Parse
             $post_require = microtime(true);
             $run_options = clone $options;
             $run = $options->namespace . '\\' . $options->class;
-            d($data);
+//            d($data);
             d($data->get('script'));
             trace();
             $main = new $run($object, $this, $data, $flags, $options);

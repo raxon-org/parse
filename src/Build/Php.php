db131c8a8e4d6323daf4332bc9486ca3d8371747
remco van der Velde <remco.pc@outlook.com>
2025-04-23 15:42:28 +0200

    implementing new parser

--- src/Build/Php.php
+++ src/Build/Php.php
@@ -3692,7 +3692,6 @@ class Php {
                 }
             }
         } else {
-            d($record);
             $value = Php::value($object, $flags, $options, $record, $record['variable']['value'],$is_set, $before, $after);
         }
         if(array_key_exists('modifier', $record['variable'])){

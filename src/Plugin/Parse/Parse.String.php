6e44202ad36610f1653f80d72acd4160c48e7383
remco van der Velde <remco.pc@outlook.com>
2025-04-24 21:34:45 +0200

    implementing new parser

--- src/Plugin/Parse/Parse.String.php
+++ src/Plugin/Parse/Parse.String.php
@@ -1,26 +1,48 @@
 <?php
 namespace Plugin;
-/**
- * @package Plugin
- * @author Remco van der Velde
- * @since 2025-02-15
- * @license MIT
- * @version 1.0
- * @changeLog
- *    - all
- */
 
+use Exception;
+use Raxon\Module\Core;
 use Raxon\Module\Data;
 
 trait Parse_String {
 
-    protected function parse_string(mixed $mixed, array | Data $data = []): mixed
+    /**
+     * @throws Exception
+     */
+    protected function parse_string(mixed $mixed, array|object $data = null): mixed
     {
         $parse = $this->parse();
-        if(!empty($parseData)){
-            return $parse->compile($mixed, $data);
-        } else {
-            return $parse->compile($mixed, []);
+
+        if($data === null){
+            $data = $this->data();
         }
+        elseif(is_array($data)){
+            $data = new Data($data);
+        }
+        elseif(is_object($data)) {
+            if(get_class($data) === Data::class){
+                $data = $data;
+            } else {
+                $data = new Data($data);
+            }
+        }
+        $options = $parse->parse_options();
+        $old_source = $options->source ?? null;
+        $hash = hash('sha256', Core::object($mixed, Core::JSON_LINE));
+        $options->source = 'Internal_' . $hash;
+        $parse->parse_options($options);
+        if(!empty($parseData)){
+            $result = $parse->compile($mixed, $data);
+        } else {
+            $result = $parse->compile($mixed, []);
+        }
+        if($old_source !== null){
+            $options->source = $old_source;
+        } else {
+            unset($options->source);
+        }
+        $parse->parse_options($options);
+        return $result;
     }
 }

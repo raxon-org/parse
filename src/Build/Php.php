bdab3c560d5af1bf55e2be4c6e50eaad2b3edafc
remco van der Velde <remco.pc@outlook.com>
2025-04-26 18:35:10 +0200

    implementing new parser

--- src/Build/Php.php
+++ src/Build/Php.php
@@ -2282,7 +2282,6 @@ class Php {
                 ) {
                     $method_value .= ')';
                 }
-                d($method_value);
                 return $method_value;
             } else {
                 $plugin = Php::plugin($object, $flags, $options, $record, str_replace('.', '_', $record['method']['name']));

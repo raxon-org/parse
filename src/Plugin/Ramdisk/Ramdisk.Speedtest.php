c7a6a81220492fea830b6a52790e2a32e0915a88
remco van der Velde <remco.pc@outlook.com>
2025-04-27 07:17:46 +0200

    implementing new parser

--- src/Plugin/Ramdisk/Ramdisk.Speedtest.php
+++ src/Plugin/Ramdisk/Ramdisk.Speedtest.php
@@ -15,7 +15,7 @@ trait Ramdisk_Speedtest {
      * @throws ObjectException|
      * @throws DirectoryCreateException
      */
-    protected function ramdisk_speedtest(): void
+    protected function ramdisk_speedtest(): string
     {
         $object = $this->object();
         $id = posix_geteuid();
@@ -26,23 +26,25 @@ trait Ramdisk_Speedtest {
             $url = $object->config('ramdisk.url') . 'speedtest';
             $command = 'dd if=/dev/zero of=' . $url . 'zero bs=4k count=100000';
             Core::execute($object, $command, $output, $notification);
-            echo 'Write:' . PHP_EOL;
+            $content = [];
+            $content[] = 'Write:' . PHP_EOL;
             if($output){
-                echo $output . PHP_EOL;
+                $content[] = $output . PHP_EOL;
             }
             if($notification){
-                echo $notification . PHP_EOL;
+                $content[] = $notification . PHP_EOL;
             }
             $command = 'dd if=' . $url . 'zero of=/dev/null bs=4k count=100000';
             Core::execute($object, $command, $output, $notification);
-            echo 'Read:' . PHP_EOL;
+            $content[] = 'Read:' . PHP_EOL;
             if($output){
-                echo $output . PHP_EOL;
+                $content[] = $output . PHP_EOL;
             }
             if($notification){
-                echo $notification . PHP_EOL;
+                $content[] = $notification . PHP_EOL;
             }
             File::delete($url . 'zero');
+            return implode('', $content);
         }
     }
 }

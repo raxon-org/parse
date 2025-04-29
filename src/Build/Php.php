b81a8e2ef5604e5d769a34ed3c72c5426df17e26
remco van der Velde <remco.pc@outlook.com>
2025-04-29 14:08:44 +0200

    implementing new parser

--- src/Build/Php.php
+++ src/Build/Php.php
@@ -3548,6 +3548,7 @@ class Php {
                 $after = [];
                 $data[] = 'try {';
                 $array_notation = Php::value($object, $flags, $options, $record, $record['variable']['array_notation'], $is_set, $before, $after);
+                $array_notation = explode('][', substr($array_notation, 1, -1));
                 ddd($array_notation);
                 $variable_value = '$data->get(\'' . $record['variable']['name'] . '\')' .  $array_notation;
                 $separator = $object->config('package.raxon/parse.build.state.separator');

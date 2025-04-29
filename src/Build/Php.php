54b5cfdb00c99cfcee69bb8efff4de656febb947
remco van der Velde <remco.pc@outlook.com>
2025-04-29 15:59:14 +0200

    implementing new parser

--- src/Build/Php.php
+++ src/Build/Php.php
@@ -2723,7 +2723,7 @@ class Php {
         $inner_reverse[] = 'elseif(is_array(' . $to_check . '[' . implode('][', $array) . '])){';
         while($check_array = array_pop($array)){
             $inner_reverse[] = rtrim('if(is_object(' . $to_check . '->' . implode('->', $array),'->') . ')){';
-            $inner_reverse[] = rtrim('elseif(is_array(' . $to_check . '[' . implode('][', $array),'[') . '])){';
+            $inner_reverse[] = rtrim('elseif(is_array(' . $to_check . '[' . implode('][', $array) . ']','[') . ')){';
         }
         krsort($inner_reverse);
         ddd($inner_reverse);

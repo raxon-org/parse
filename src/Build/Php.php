9e287f74f6846743484918ed116fb7ba6f402fbb
remco van der Velde <remco.pc@outlook.com>
2025-04-30 15:08:36 +0200

    implementing new parser

--- src/Build/Php.php
+++ src/Build/Php.php
@@ -2768,6 +2768,8 @@ class Php {
                         array_key_exists('array_notation', $record) &&
                         !empty($record['array_notation'])
                     ){
+                        $try_catch = $object->config('package.raxon/parse.build.state.try_catch');
+                        $separator = $object->config('package.raxon/parse.build.state.separator');
                         $array_notation = Php::value($object, $flags, $options, $record, $record['array_notation'], $is_set, $before, $after);
                         $array_notation = explode('][', substr($array_notation, 1, -1));
                         $root_uuid = Core::uuid_variable();

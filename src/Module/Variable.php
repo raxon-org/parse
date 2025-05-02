408c8a22b19d87ff79ef1ac80e597a28ac7187fd
remco van der Velde <remco.pc@outlook.com>
2025-05-02 21:19:28 +0200

    implementing new parser

--- src/Module/Variable.php
+++ src/Module/Variable.php
@@ -307,7 +307,6 @@ class Variable
                                         ':',
                                         '_',
                                         '#',
-                                        ']'
                                     ],
                                     true
                                 )

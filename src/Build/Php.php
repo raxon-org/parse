df567f954e905a95efdfa3a92111176888d44131
remco van der Velde <remco.pc@outlook.com>
2025-05-03 18:39:21 +0200

    implementing new parser

--- src/Build/Php.php
+++ src/Build/Php.php
@@ -741,11 +741,10 @@ class Php {
                                 $content[$if_method][$elseif_count - 1]['content'][$row_nr][] = $record;
                             } else {
                                 if(!array_key_exists($if_method, $content)){
-                                    d($options->source);
-                                    d(trace());
-                                    d($if_method);
-                                    d($record);
-                                    ddd($content);
+                                    $content[$if_method] = [];
+                                }
+                                if(!array_key_exists('content', $content[$if_method])){
+                                    $content[$if_method]['content'] = [];
                                 }
                                 if(!array_key_exists($row_nr, $content[$if_method]['content'])){
                                     $content[$if_method]['content'][$row_nr] = [];

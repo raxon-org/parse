3b1758d0493d06142dd1f772161e6713e0e4ff59
remco van der Velde <remco.pc@outlook.com>
2025-05-07 08:02:45 +0200

    implementing new parser

--- src/Build/Php.php
+++ src/Build/Php.php
@@ -775,8 +775,6 @@ class Php {
                             $content['if'] = [];
                             $content['elseif'] = [];
                             $content['else'] = [];
-                            d($if_method);
-                            d($content);
                         } else {
                             if($if_method === 'elseif'){
                                 if(!array_key_exists($row_nr, $content[$if_method][$elseif_count - 1]['content'])){

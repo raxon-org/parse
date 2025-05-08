937acb24751ee09efb1b1bfa86f64d7ca9701aec
remco van der Velde <remco.pc@outlook.com>
2025-05-08 18:41:48 +0200

    implementing new parser

--- src/Plugin/String/String.Uppercase.First.php
+++ src/Plugin/String/String.Uppercase.First.php
@@ -6,13 +6,11 @@ trait String_Uppercase_First {
     protected function string_uppercase_first(string $string): string
     {
         //php 8.4
-        d($string);
         if(function_exists('mb_ucfirst')){
             $result =  mb_ucfirst($string);
         } else {
             $result = ucfirst($string);
         }
-        d($result);
         return $result;
     }
 

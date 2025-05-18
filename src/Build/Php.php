9bb62c340ba7358135f3fb3a3222a61404d2be4d
remco van der Velde <remco.pc@outlook.com>
2025-05-18 17:08:36 +0200

    info output

--- src/Build/Php.php
+++ src/Build/Php.php
@@ -1689,7 +1689,7 @@ class Php {
                             $text = str_replace('\\&', $ampersand_uuid, $text);
                             $text = str_replace('&quot;', $double_quote_uuid, $text);
                             $text = str_replace('&apos;', $single_quote_uuid, $text);
-                            $token = Token::tokenize($object, $flags, $options, substr($record['text'], 2, -2));
+                            $token = Token::tokenize($object, $flags, $options, substr($text, 2, -2));
                             $token = Php::document_tag_prepare($object, $flags, $options, $token);
                             $embed = Php::document_tag($object, $flags, $options, $token);
                             if(property_exists($options, 'variable')){

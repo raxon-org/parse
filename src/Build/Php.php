370548fe4003c486fdc97d3ab4fe72f7bf9a222b
remco van der Velde <remco.pc@outlook.com>
2025-05-18 13:57:57 +0200

    info output

--- src/Build/Php.php
+++ src/Build/Php.php
@@ -96,6 +96,9 @@ class Php {
         $document[] = ' * @note compiled by ' . $object->config('framework.name') . ' ' . $object->config('framework.version');
         $document[] = ' * @url ' . $object->config('framework.url');
         $document[] = ' * @source ' . $source;
+        if(property_exists($options, 'source_root')){
+            $document[] = ' * @source-root ' . $options->source_root;
+        }
         $document[] = ' */';
         $document[] = '';
         $document[] = 'namespace Package\Raxon\Parse;';

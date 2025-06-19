3469efbfdfe5b89ab6b3d464867c70a207f6533d
remco van der Velde <remco.pc@outlook.com>
2025-06-19 10:41:50 +0200

    xp

--- src/Package/Test/Php/1-100/018.Bug.Shmop.php
+++ src/Package/Test/Php/1-100/018.Bug.Shmop.php
@@ -41,7 +41,7 @@ try {
     $count = count($chars);
 
     $words = [];
-    for($i = 0; $i < 10000000; $i++){
+    for($i = 0; $i < 100000000; $i++){
         $words[] = random_word($chars, $count);
     }
     $write = implode(' ', $words);

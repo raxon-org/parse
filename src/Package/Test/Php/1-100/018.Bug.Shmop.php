29d85c4f84cc7f6b3dbfe7ab39a6113818ab13d9
remco van der Velde <remco.pc@outlook.com>
2025-06-19 10:28:43 +0200

    xp

---
+++ src/Package/Test/Php/1-100/018.Bug.Shmop.php
@@ -0,0 +1,86 @@
+<?php
+/**
+ * @author          Remco van der Velde
+ * @since           2020-10-27
+ * @version         1.0
+ * @changeLog
+ *     -    all
+ */
+
+use Raxon\App;
+use Raxon\Config;
+use Raxon\Module\Dir;
+use Raxon\Module\File;
+
+use Raxon\Exception\LocateException;
+use Raxon\Exception\ObjectException;
+use Raxon\Exception\FileMoveException;
+
+$dir = __DIR__;
+$dir_vendor =
+    DIRECTORY_SEPARATOR .
+    'Application' .
+    DIRECTORY_SEPARATOR .
+    'vendor' .
+    DIRECTORY_SEPARATOR;
+
+$autoload = $dir_vendor . 'autoload.php';
+$autoload = require $autoload;
+try {
+    $config = new Config(
+        [
+            'dir.vendor' => $dir_vendor,
+            'time.start' => microtime(true),
+        ]
+    );
+    $app = new App($autoload, $config);
+
+    $chars = chars();
+
+    $wordlength = rand(2, 25);
+    ddd($wordlength);
+
+
+
+
+
+
+} catch (Exception | LocateException | ObjectException $exception) {
+    echo $exception;
+}
+
+function chars(): array
+{
+    $chars = [];
+    $chars[] = 'a';
+    $chars[] = 'b';
+    $chars[] = 'c';
+    $chars[] = 'd';
+    $chars[] = 'e';
+    $chars[] = 'f';
+    $chars[] = 'g';
+    $chars[] = 'h';
+    $chars[] = 'i';
+    $chars[] = 'j';
+    $chars[] = 'k';
+    $chars[] = 'l';
+    $chars[] = 'm';
+    $chars[] = 'n';
+    $chars[] = 'o';
+    $chars[] = 'p';
+    $chars[] = 'q';
+    $chars[] = 'r';
+    $chars[] = 's';
+    $chars[] = 't';
+    $chars[] = 'u';
+    $chars[] = 'v';
+    $chars[] = 'w';
+    $chars[] = 'x';
+    $chars[] = 'y';
+    $chars[] = 'z';
+
+    foreach($chars as $char){
+        $chars[] = strtoupper($char);
+    }
+    return $chars;
+}

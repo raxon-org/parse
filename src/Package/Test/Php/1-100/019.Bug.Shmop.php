fe6be9ca5f14480d2985499450505442b405c057
remco van der Velde <remco.pc@outlook.com>
2025-06-20 15:01:20 +0200

    xp

--- src/Package/Test/Php/1-100/019.Bug.Shmop.php
+++ src/Package/Test/Php/1-100/019.Bug.Shmop.php
@@ -98,6 +98,7 @@ function time_format(float|int $seconds=0, string $string='in', $compact=false):
     $explode = explode('.', $seconds);
     $msec = $explode[1] ?? 0;
     $seconds = $seconds % 60;
+    ddd($seconds);
     if($days > 0){
         if($compact){
             $string .= $days . ' ' . 'd' . ' ';

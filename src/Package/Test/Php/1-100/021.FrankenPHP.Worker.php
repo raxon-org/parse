0a168e3a800234c4060e0af553f0a7fe09dd07e3
remco van der Velde <remco.pc@outlook.com>
2025-06-21 16:07:41 +0200

    php 8.4 deprecations

---
+++ src/Package/Test/Php/1-100/021.FrankenPHP.Worker.php
@@ -0,0 +1,89 @@
+<?php
+/**
+ * @author          Remco van der Velde
+ * @since           2020-10-27
+ * @version         1.0
+ * @changeLog
+ *     -    all
+ */
+
+use Raxon\App;
+use Raxon\Config;
+use Raxon\Module\Dir;
+use Raxon\Module\File;
+
+use Raxon\Exception\LocateException;
+use Raxon\Exception\ObjectException;
+use Raxon\Exception\FileMoveException;
+
+$dir = __DIR__;
+$dir_vendor =
+    DIRECTORY_SEPARATOR .
+    'Application' .
+    DIRECTORY_SEPARATOR .
+    'vendor' .
+    DIRECTORY_SEPARATOR;
+
+$autoload = $dir_vendor . 'autoload.php';
+$autoload = require $autoload;
+try {
+    $config = new Config(
+        [
+            'dir.vendor' => $dir_vendor,
+            'time.start' => microtime(true),
+        ]
+    );
+    $app = new App($autoload, $config);
+
+    while (frankenphp_handle_request()) {
+        // Handle each request with the already booted application
+        $response = $app->request;
+        var_dump($response);
+    }
+
+
+} catch (Exception | LocateException | ObjectException $exception) {
+    echo $exception;
+}
+
+/**
+ * @throws FileMoveException
+ */
+function batch($list=[]): void
+{
+    while(true){
+        $dir = new Dir();
+        foreach($list as $nr => $url){
+            $read = $dir->read($url, true);
+            foreach($read as $nr => $file) {
+                $explode = explode('.', $file->url);
+                $extension = array_pop($explode);
+                $file->new = false;
+                $file->temp = false;
+                if(strtoupper($extension) === 'WMA'){
+                    $file->new = implode('.', $explode) . '.mp3';
+                }
+                elseif(strtoupper($extension) === 'WAV'){
+                    $file->new = implode('.', $explode) . '.mp3';
+                }
+                if($file->new !== false){
+                    $file->new = str_replace(['\'', '"'], '', $file->new);
+                    if(File::exist($file->new)){
+                        continue;
+                    }
+                    File::move($file->url, $file->url . '.temp');
+                    $command = 'ffmpeg -i "' .
+                        str_replace(['(', ')', '"'], ['(', ')', '\\"'], $file->url) . '.temp' .
+                        '" -vn -ar 44100 -ac 2 -ab 320k -f mp3 "' .
+                        str_replace(['(', ')', '\''], ['(', ')', '\\"'], $file->new) .
+                        '"';
+                    echo $command . PHP_EOL;
+                    exec($command);
+                    File::move($file->url . '.temp', $file->url);
+                }
+            }
+        }
+        sleep(5);
+    }
+
+}

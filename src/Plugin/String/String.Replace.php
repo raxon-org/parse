5cf04ef5257ce3e4fa86f74666f4635b6c777e6e
remco van der Velde <remco.pc@outlook.com>
2025-06-22 01:16:01 +0200

    php 8.4 deprecations

--- src/Plugin/String/String.Replace.php
+++ src/Plugin/String/String.Replace.php
@@ -6,7 +6,7 @@ use Raxon\Parse\Attribute\Argument;
 trait String_Replace {
 
     #[Argument(apply: "literal", count: 1, index:3)]
-    protected function string_replace(array|string $subject='', array|string $search='', array|string $replace='', string $attribute=null): array|string
+    protected function string_replace(array|string $subject='', array|string $search='', array|string $replace='', string|null $attribute=null): array|string
     {
         if(!empty($attribute)){
             $attribute = trim($attribute, '\'"');

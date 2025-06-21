e4b0053a5d6f5d80e017161b0d3cb35e4e69f25f
remco van der Velde <remco.pc@outlook.com>
2025-06-21 23:12:08 +0200

    php 8.4 deprecations

--- src/Package/Test/Php/1-100/021.FrankenPHP.Worker.php
+++ src/Package/Test/Php/1-100/021.FrankenPHP.Worker.php
@@ -35,12 +35,13 @@ try {
     $handler = static function () use ($app) {
         // Called when a request is received,
         // superglobals, php://input and the like are reset
-        var_dump($_GET);
-        var_dump($_POST);
-        var_dump($_COOKIE);
-        var_dump($_FILES);
-        var_dump($_SERVER);
-        var_dump($app->request());
+        echo App::run($app);
+//        var_dump($_GET);
+//        var_dump($_POST);
+//        var_dump($_COOKIE);
+//        var_dump($_FILES);
+//        var_dump($_SERVER);
+//        var_dump($app->request());
     };
     $count = 0;
     $max = 5;

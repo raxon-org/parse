41e4e4b52ee7c263a157e6c4ac51c75c1b626407
remco van der Velde <remco.pc@outlook.com>
2025-06-30 18:39:30 +0200

    init

--- src/Build/Php.php
+++ src/Build/Php.php
@@ -2001,12 +2001,15 @@ class Php {
             $ampersand_uuid = Core::uuid_variable();
             $double_quote_uuid = Core::uuid_variable();
             $single_quote_uuid = Core::uuid_variable();
+            $backslash_uuid = Core::uuid_variable();
             $text = str_replace('\\&', $ampersand_uuid, $text);
             $text = str_replace('&quot;', $double_quote_uuid, $text);
             $text = str_replace('&apos;', $single_quote_uuid, $text);
+            $text = str_replace('\\', $backslash_uuid, $text);
             $text = str_replace($ampersand_uuid, '&', $text);
             $text = str_replace($double_quote_uuid, '"', $text);
             $text = str_replace($single_quote_uuid, '\'', $text);
+            $text = str_replace($backslash_uuid, '\\\\', $text);
             return$text;
         }
         return false;

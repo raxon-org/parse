ebc2ffb70e47342cc901c35207992af72d789af0
remco van der Velde <remco.pc@outlook.com>
2025-07-02 11:46:39 +0200

    Refactor Parse class to remove unnecessary debug checks and streamline input processing

--- src/Module/Parse.php
+++ src/Module/Parse.php
@@ -727,12 +727,8 @@ class Parse
                 $object->config('dictionary.view') .
                 $object->config('ds')
             ;
-            Dir::create($dir, Dir::CHMOD);
-            if($is_debug){
-                $input = Token::literal_apply($object, $flags, $options, $input);
-                // d($object->data('literal'));                
-            }
-            
+            Dir::create($dir, Dir::CHMOD);            
+            $input = Token::literal_apply($object, $flags, $options, $input);
             $token = Token::tokenize($object, $flags, $options, $input);            
             if($is_debug){
             //    ddd($token);

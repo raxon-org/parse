3637419858338b7a6eee8adc0eab729f044c2ad2
remco van der Velde <remco.pc@outlook.com>
2025-07-06 16:17:55 +0200

    init

--- src/Build/Php.php
+++ src/Build/Php.php
@@ -2910,6 +2910,7 @@ class Php {
         $source = $options->source ?? '';
         $value = '';
         $skip = 0;
+        d($input);
         $input = Php::value_set($object, $flags, $options, $input, $is_set, $count);
         $input = Variable::modifier($object, $flags, $options, $input, $tag);
         foreach ($input['array'] as $nr => $record) {

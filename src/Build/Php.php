6aa113d97e88db12a4ddfe17174c3f5248fd5597
remco van der Velde <remco.pc@outlook.com>
2025-07-06 16:59:30 +0200

    init

--- src/Build/Php.php
+++ src/Build/Php.php
@@ -3269,12 +3269,22 @@ class Php {
                 ) {
                     $value .= $record['value'];
                 }
-                elseif(array_key_exists('type', $record) &&
+                elseif(
+                    array_key_exists('type', $record) &&
                     $record['type'] === 'symbol'
                 ){
                     $value .= $record['value'];
                 }
-                else {
+                elseif(
+                    array_key_exists('type', $record) &&
+                    $record['type'] === 'set'
+                ){
+                    $set = [
+                        'string' => $record['value'],
+                        'array' => $record['array']
+                    ];
+                    $value .= Php::value($object, $flags, $options, $tag, $set, $is_set_right, $before, $after);                                                                                                                                               
+                } else {
                     $value .= $record['value'];
                     d('not implemented');
                     ddd($record);

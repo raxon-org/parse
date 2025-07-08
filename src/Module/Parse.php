fa8c5e729a2acbc7a2f33d9a941c3c937ff5a319
remco van der Velde <remco.pc@outlook.com>
2025-07-08 11:42:12 +0200

    init

--- src/Module/Parse.php
+++ src/Module/Parse.php
@@ -738,6 +738,7 @@ class Parse
             ;
             Dir::create($dir, Dir::CHMOD);            
             $input = Token::literal_apply($object, $flags, $options, $input);
+            d($input);
             $token = Token::tokenize($object, $flags, $options, $input);            
             if($is_debug){
             //    ddd($token);

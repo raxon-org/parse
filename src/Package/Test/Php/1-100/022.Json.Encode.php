ac12eae7e6af20720dd0ee4f6b88956eea88aa7f
remco van der Velde <remco.pc@outlook.com>
2025-07-17 06:39:37 +0200

    bug #22

--- src/Package/Test/Php/1-100/022.Json.Encode.php
+++ src/Package/Test/Php/1-100/022.Json.Encode.php
@@ -1,1 +1,9 @@
 <?php
+$array = [  
+  (object) [
+    'role' => 'user',
+    'content' => "{{json.encode(file.read('/mnt/Vps3/Mount/Shared/Plugin/Host/Host.Domain.php'))}}" 
+  ]
+];
+
+var_dump($array);

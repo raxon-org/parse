ccdf349b925e18aa1c3cb4a55d3e897305bf32bc
remco van der Velde <remco.pc@outlook.com>
2025-07-17 09:57:51 +0200

    init

--- src/Module/Token.php
+++ src/Module/Token.php
@@ -106,6 +106,7 @@ class Token
         if($tags === false){
             $tags = Tag::define($object, $flags, $options, $input);
             $tags = Tag::remove($object, $flags, $options, $tags);
+            d($tags);
             $tags = Token::abstract_syntax_tree($object, $flags, $options, $tags);
             d($tags);
             $is_new = true;

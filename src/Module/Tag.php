ed2fa7e7bf9e22603a3c07f4b0fad32f7a6ea194
remco van der Velde <remco.pc@outlook.com>
2025-07-17 09:56:21 +0200

    init

--- src/Module/Tag.php
+++ src/Module/Tag.php
@@ -735,8 +735,7 @@ class Tag
                 }
                 $tag_list[$line][] = $record;
             }
-        }
-        d($tag_list);
+        }        
         return $tag_list;
     }
 

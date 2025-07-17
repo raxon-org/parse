3f67077cb7aad180320fc8298721f9d533afe3b2
remco van der Velde <remco.pc@outlook.com>
2025-07-17 07:06:20 +0200

    init

--- src/Package/Test/Php/1-100/022.Json.Encode.php
+++ src/Package/Test/Php/1-100/022.Json.Encode.php
@@ -1,12 +1,57 @@
 <?php
-$array = [  
-  (object) [
-    'role' => 'user',
-    'content' => "{{json.encode(file.read('/mnt/Vps3/Mount/Shared/Plugin/Host/Host.Domain.php'))}}" 
-  ]
-];
+/**
+ * @author          Remco van der Velde
+ * @since           2020-10-27
+ * @version         1.0
+ * @changeLog
+ *     -    all
+ */
 
-$json = json_encode($array);
+use Raxon\App;
+use Raxon\Config;
+use Raxon\Module\Dir;
+use Raxon\Module\File;
+use Raxon\Module\Data;
+use Raxon\Parse\Module\Parse;
 
-var_dump($array);
-var_dump($json);
+use Raxon\Exception\LocateException;
+use Raxon\Exception\ObjectException;
+use Raxon\Exception\FileMoveException;
+
+$dir = __DIR__;
+$dir_vendor =
+    DIRECTORY_SEPARATOR .
+    'Application' .
+    DIRECTORY_SEPARATOR .
+    'vendor' .
+    DIRECTORY_SEPARATOR;
+
+$autoload = $dir_vendor . 'autoload.php';
+$autoload = require $autoload;
+try {
+    $config = new Config(
+        [
+            'dir.vendor' => $dir_vendor,
+            'time.start' => microtime(true),
+        ]
+    );
+    $app = new App($autoload, $config);
+
+    $array = [  
+        (object) [
+            'role' => 'user',
+            'content' => "{{json.encode(file.read(\"/mnt/Vps3/Mount/Shared/Plugin/Host/Host.Domain.php\"))}}" 
+        ]
+    ];
+    $json = json_encode($array);
+
+    $options = (object) [
+        'source' => 'internal_test'
+    ];
+    $data = new Data($app->data());    
+    $parse = new Parse($object, $data, App::flags($app), $options);
+    $output = $parse->compile($json, $data);
+    ddd($output);
+} catch (Exception | LocateException | ObjectException $exception) {
+    echo $exception;
+}

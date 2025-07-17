47ac4df3ffb6511e84c9f14232dff8aabd36d4d5
remco van der Velde <remco.pc@outlook.com>
2025-07-17 21:54:43 +0200

    init

--- src/Module/Parse.php
+++ src/Module/Parse.php
@@ -469,10 +469,8 @@ class Parse
                         $data->set($key_parent, $parentNode);
                     }
                 }
-                $parse_data = clone $data;                  
-                d($json);                                         
-                $json = $parse->compile($json, $parse_data, $is_debug);  
-                d($json);                              
+                $parse_data = clone $data;                                  
+                $json = $parse->compile($json, $parse_data, $is_debug);                                               
                 $input = Core::object($json, Core::OBJECT);                
                 $data->set('this.' . $object->config('package.raxon/parse.object.this.key', null));
                 return $input;

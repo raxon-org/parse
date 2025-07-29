8636cd72cdaf084d5284d27abf62e913323b30ef
remco van der Velde <remco.pc@outlook.com>
2025-07-29 15:49:42 +0200

    init

--- src/Plugin/Server/Server.Url.php
+++ src/Plugin/Server/Server.Url.php
@@ -15,9 +15,7 @@ trait Server_Url {
     public function server_url(string $name): ?string
     {
         $object = $this->object();
-        $name = str_replace('.', '-', $name);
-        d($name);
-        ddd($object->config('server.url'));
+        $name = str_replace('.', '-', $name);        
         $url = $object->config('server.url.' . $name . '.' . $object->config('framework.environment'));
         if(
             $url &&

8045d798febec4db4bdd3dea613f20df2f82b0b2
remco van der Velde <remco.pc@outlook.com>
2025-07-29 18:22:01 +0200

    init

--- src/Plugin/Route/Route.Get.php
+++ src/Plugin/Route/Route.Get.php
@@ -8,9 +8,10 @@ trait Route_Get {
     /**
      * @throws Exception
      */
-    protected function route_get(string $name='', array $options=[]): bool | string
+    protected function route_get(string|object $name='', array $options=[]): bool | string
     {
         $object = $this->object();
+        ddd($name);
         return $object->route()::find($object, $name, $options);
     }
 }

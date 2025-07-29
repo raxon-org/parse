38a35d6559b3e5f7abf6d95e7a6d6939e0c2d037
remco van der Velde <remco.pc@outlook.com>
2025-07-29 18:31:09 +0200

    init

--- src/Plugin/Route/Route.Get.php
+++ src/Plugin/Route/Route.Get.php
@@ -10,10 +10,12 @@ trait Route_Get {
      */
     protected function route_get(string|object $name='', array $options=[]): bool | string
     {
-        $object = $this->object();
-        if($name === ''){
+        $object = $this->object();        
+        $find = $object->route()::find($object, $name, $options);
+        if($find === false || $find === ''){
+            d($name);
             trace();
-        }             
-        return $object->route()::find($object, $name, $options);
+            die('here');
+        }
     }
 }

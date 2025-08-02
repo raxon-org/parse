08fd61ce5f8ae94e3faea18c0c330366291a50a3
remco van der Velde <remco.pc@outlook.com>
2025-08-02 15:34:23 +0200

    init

--- src/Plugin/Dir/Dir.Read.php
+++ src/Plugin/Dir/Dir.Read.php
@@ -8,6 +8,7 @@ trait Dir_Read {
 
     protected function dir_read(string $directory='', bool $recursive=false, string $format='flat'): array
     {
+        d($directory);
         if(File::exist($directory)){
             $dir = new Dir();
             return $dir->read($directory, $recursive, $format);

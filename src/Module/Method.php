6c94c920789f296cf06aad033d3939c2a5d13edb
remco van der Velde <remco.pc@outlook.com>
2025-08-02 20:53:59 +0200

    init

--- src/Module/Method.php
+++ src/Module/Method.php
@@ -265,7 +265,7 @@ class Method
                         ];
                         if($is_variable_method === true){
                             $call_type = '::';
-                            d($input['array'][$is_method]);
+                            d($input['array']);
                             ddd($name);
                             $explode = explode($call_type, $name, 2);
                             if(array_key_exists(1, $explode)){

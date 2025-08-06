b961a9564f1eb444ef8647bafde0317dd52c843c
remco van der Velde <remco.pc@outlook.com>
2025-08-06 19:04:55 +0200

    init

--- src/Module/Parse.php
+++ src/Module/Parse.php
@@ -749,7 +749,8 @@ class Parse
             ;
             Dir::create($dir, Dir::CHMOD);            
             $input = Token::literal_apply($object, $data, $flags, $options, $input);            
-            $token = Token::tokenize($object, $flags, $options, $input);              
+            $token = Token::tokenize($object, $flags, $options, $input);  
+            d($token);            
             if($is_debug){
                 // d($input);
                 // d($token);

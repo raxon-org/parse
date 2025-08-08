ab4274bae8eeb35d0cf87bda68b677450aa5795a
remco van der Velde <remco.pc@outlook.com>
2025-08-08 20:56:13 +0200

    trace

--- src/Module/Token.php
+++ src/Module/Token.php
@@ -1785,8 +1785,9 @@ class Token
         if($cache->has($hash)){
             $input = $cache->get($hash);
         } else {
-//            d($input);
+           d($input);
             $input = Symbol::define($object, $flags, $options, $input);
+            d($input);
 //            $input = Token::remove_comment($object, $flags, $options, $input);
 //            breakpoint($input);
             $input = Cast::define($object, $flags, $options, $input);

77de8dbc373d6fd98a08d84a4fb2db0c702d405f
remco van der Velde <remco.pc@outlook.com>
2025-08-22 15:40:37 +0200

    init

--- src/Plugin/Block/Block.Markdown.php
+++ src/Plugin/Block/Block.Markdown.php
@@ -8,26 +8,19 @@ use Package\Raxon\Markdown\Module as Module;
 trait Block_Markdown {
 
     #[Argument(apply: "literal", count: 1, index:1)]
-    protected function block_markdown(string|null $value=null, string|array $name='', array $config=[]): string
+    protected function block_markdown(string|null $value=null, string|null $name=null, array $config=[]): string
     {
         $data = $this->data();
-        $object = $this->object();
-        if(is_array($name)){
-            $config = $name;
-            $name = '';
-        }
-        d($value);
-        d($config);
-        ddd($name);
+        $object = $this->object();                
         $parser = new Module\Markdown();
-        $value = $parser->parse($object, $value, $config);
-        $name = trim($name,'\'"');
-        if(empty($name)){
+        $value = $parser->parse($object, $value, $config);        
+        if($name === null){
             $content = $data->data('#content');
             $content[] = $value;
             $data->data('#content', $content);
             return $value;
         } else {
+            $name = trim($name,'\'"');
             if(substr($name, 0, 1) === '$'){
                 $name = substr($name, 1);
             }

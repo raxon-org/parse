04f594e296e4297862d7826463f5de3759b70a53
remco van der Velde <remco.pc@outlook.com>
2025-08-22 15:34:47 +0200

    better

--- src/Plugin/Block/Block.Markdown.php
+++ src/Plugin/Block/Block.Markdown.php
@@ -8,14 +8,16 @@ use Package\Raxon\Markdown\Module as Module;
 trait Block_Markdown {
 
     #[Argument(apply: "literal", count: 1, index:1)]
-    protected function block_markdown(string|null $value=null, string $name=''): string
+    protected function block_markdown(string|null $value=null, string|array $name='', array $config=[]): string
     {
         $data = $this->data();
         $object = $this->object();
+        if(is_array($name)){
+            $config = $name;
+            $name = '';
+        }
         $parser = new Module\Markdown();
-        $value = $parser->parse($object, $value, [
-            'html_input' => 'allow'
-        ]);
+        $value = $parser->parse($object, $value, $config);
         $name = trim($name,'\'"');
         if(empty($name)){
             $content = $data->data('#content');

ad77cc167821f16e3b8686c4dca5743e603fd19c
Remco van der Velde <remco@universeorange.com>
2025-08-30 21:05:38 +0200

    init

--- src/Module/Parse.php
+++ src/Module/Parse.php
@@ -797,6 +797,7 @@ class Parse
             $result = $main->run();
             //d($result);
             d($data->get('script'));
+            d($object->get('script'));
 //            d($url_php);
 //            d($data->get('script'));
 //            d($this->data()->get('script'));

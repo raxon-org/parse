c6488e8db4ac7e6f0784681d7f6189c441c722d2
Remco van der Velde <remco@universeorange.com>
2025-12-30 17:24:15 +0100

    init workspace

--- src/Module/Token.php
+++ src/Module/Token.php
@@ -55,6 +55,8 @@ class Token
     {
         $uuid_start = $object->config('literal.single.quote.start');
         $uuid_end = $object->config('literal.single.quote.end');
+        d($uuid_start);
+        d($uuid_end);
         if(is_string($input)){
             $input = str_replace($uuid_start, '{{literal}}', $input);
             $input = str_replace($uuid_end, '{{/literal}}', $input);

856c12f182928e995e3acc2249a4f0e7d1da8b80
Remco van der Velde <remco@universeorange.com>
2025-12-30 16:20:58 +0100

    init workspace

--- src/Build/Php.php
+++ src/Build/Php.php
@@ -640,6 +640,7 @@ class Php {
                         $record['marker']['is_close'] === true
                     ){
                         $text = [];
+                        d($marker_data);
                         foreach($marker_data as $marker_record){
                             if(array_key_exists('text', $marker_record)){
                                 $text[] = $marker_record['text'];

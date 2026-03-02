1e5834d8b6b78f2647b7cd1329e2537593396af6
Remco van der Velde <remco@universeorange.com>
2026-03-02 18:08:30 +0100

    init

--- src/Module/Parse.php
+++ src/Module/Parse.php
@@ -752,7 +752,8 @@ class Parse
             $input = Token::literal_single_quote_apply($object, $data, $flags, $options, $input);
             $input = Token::literal_apply($object, $data, $flags, $options, $input);
             $input = Token::literal_single_quote_restore($object, $data, $flags, $options, $input);
-            $token = Token::tokenize($object, $flags, $options, $input);                      
+            $token = Token::tokenize($object, $flags, $options, $input);
+            d($token);
             if($is_debug){
                 // d($input);
                 // d($token);

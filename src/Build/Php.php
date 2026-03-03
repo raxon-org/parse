ec2e7ce6fafb37b9e28da77206cff307f4bca79a
Remco van der Velde <remco@universeorange.com>
2026-03-03 18:17:07 +0100

    init

--- src/Build/Php.php
+++ src/Build/Php.php
@@ -2827,6 +2827,7 @@ class Php {
                         $argument = $name . '()';
                     }
                 } else {
+                    d($argument);
                     $argument = Php::value($object, $flags, $options, $record, $argument, $is_set, $before, $after);
                     d($argument);
 //                    $uuid_variable = Core::uuid_variable();

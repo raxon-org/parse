1f9431168cd1b6db2f7703653df0419d3d7f9ae1
Remco van der Velde <remco@universeorange.com>
2026-03-03 11:38:47 +0100

    init

--- src/Build/Php.php
+++ src/Build/Php.php
@@ -2761,7 +2761,7 @@ class Php {
                     if(array_key_exists($nr, $argument_is_reference)){
                         $argument['array'][0]['is_reference'] = true;
                     }
-                    breakpoint($argument);
+//                    breakpoint($argument);
                     $argument = Php::value($object, $flags, $options, $record, $argument, $is_set, $before, $after);
                     $uuid_variable = Core::uuid_variable();
                     $before[] = $uuid_variable . ' = ' . $argument . ';';

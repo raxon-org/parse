9b7a878675a7a156f4c83a5ca3e2dcad0dc48d3e
Remco van der Velde <remco@universeorange.com>
2026-03-04 19:00:14 +0100

    init

--- src/Build/Php.php
+++ src/Build/Php.php
@@ -3200,6 +3200,7 @@ class Php {
             switch($value){
                 case ' + ':
                     $value_transform = Php::value_transform($object, $flags, $options, $value_array, $nr, $before, $after);
+                    d($value_transform);
                     $value_array = $value_transform['value'];
                     $value_array[$nr] = '$this->value_plus(' . implode('', $value_transform['left']) . ',' . implode('', $value_transform['right']) . ')';// . implode('', $right['value']);
                     $is_nested = true;

8a28eab0965e762c012a281e94c923d48d35a5da
Remco van der Velde <remco@universeorange.com>
2026-03-04 19:29:27 +0100

    init

--- src/Build/Php.php
+++ src/Build/Php.php
@@ -3200,6 +3200,7 @@ class Php {
     private static function value_activate_symbol(App $object, $flags, $options, $value_array, &$before=[], &$after=[]): array
     {
         $is_nested = false;
+        d($value_array);
         foreach($value_array as $nr => $value){
             switch($value){
                 case ' + ':

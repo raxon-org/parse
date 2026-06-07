28415d6d72ca4d4153d1e54deb34bf87599a0dc5
Remco van der Velde <remco@universeorange.com>
2026-06-07 20:20:07 +0200

    init

--- src/Module/Parse.php
+++ src/Module/Parse.php
@@ -607,7 +607,7 @@ class Parse
 //                    $this->parse_set_options($options);
                     $data->set('this.' . $object->config('package.raxon/parse.object.this.property'), $key);
                     $data->set('this.' . $object->config('package.raxon/parse.object.this.attribute'), $key);
-                    $this->local($depth, $input);
+                    $input = $this->local($depth, $input);
 //                    $data->set( $key_parent . '.#depth', $depth);
                     $parentNode = null;
                     if($depth === 0){

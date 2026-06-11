12ee249d58a40666498a5003fb2bcdb0f989aff2
Remco van der Velde <remco@universeorange.com>
2026-06-11 19:29:04 +0200

    init

---
+++ src/Plugin/Type.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * @package Plugin\Type
+ * @author Remco van der Velde
+ * @since 2024-08-19
+ * @license MIT
+ * @version 1.0
+ * @changeLog
+ *    - all
+ */
+namespace Raxon\Parse\Plugin;
+
+trait Type {
+
+    public function type(mixed $value=null): string
+    {
+        return gettype($value);
+    }
+
+}

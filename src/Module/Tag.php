b9b0e18998144f875f87bee2071bb6e50606ad2f
Remco van der Velde <remco@universeorange.com>
2026-06-11 20:55:48 +0200

    init

--- src/Module/Tag.php
+++ src/Module/Tag.php
@@ -17,7 +17,6 @@ class Tag
         $length = mb_strlen($input);
         $start = microtime(true);
         $split = mb_str_split($input, 1);
-        d($split);
         $curly_count = 0;
         $line = 1;
         $column = [];
@@ -743,10 +742,12 @@ class Tag
                 $tag &&
                 $is_comment === false
             ){
-                $tag .= $char;                      
+                $tag .= $char;
+                d($tag);
             }
             elseif($is_comment === false){
                 $text .= $char;
+                d($text);
             }
             if($char !== "\n") {
                 $column[$line]++;

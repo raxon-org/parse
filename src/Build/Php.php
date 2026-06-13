30a9d527224b8c3d92734f507a23be1bc14f05fd
Remco van der Velde <remco@universeorange.com>
2026-06-13 17:34:03 +0200

    newtest

--- src/Build/Php.php
+++ src/Build/Php.php
@@ -2257,7 +2257,6 @@ class Php {
                     $before_for = [];
                     $after_for = [];
                     if($argument_count === 3){
-                        d($record['method']['argument']);
                         foreach($record['method']['argument'] as $nr => $argument){
                             if($nr > 0){
                                 $object->config('package.raxon/parse.build.state.try_catch', false);

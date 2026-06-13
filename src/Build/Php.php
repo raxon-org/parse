7d85f8c6c15a41df82c5550ed8db660008412761
Remco van der Velde <remco@universeorange.com>
2026-06-13 17:05:44 +0200

    newtest

--- src/Build/Php.php
+++ src/Build/Php.php
@@ -2274,6 +2274,7 @@ class Php {
                         }
                         $method_value[2] = str_replace($separator_uuid, ',', $method_value[2]);
                         $method_value[2] = substr($method_value[2], 0, -1);
+                        d($method_value);
                         $before[] = str_replace($separator_uuid, ';', $method_value[0]);
                         foreach($before_for as $line){
                             $before[] = str_replace($separator_uuid, ';', $line);

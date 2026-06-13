4f585fc7473d2f2a9c4a42f7da1890903e9c5fd2
Remco van der Velde <remco@universeorange.com>
2026-06-13 18:26:28 +0200

    newtest

--- src/Module/Variable.php
+++ src/Module/Variable.php
@@ -447,6 +447,7 @@ class Variable
         if(array_key_exists('array', $input) === false){
             return $input;
         }
+        d($input);
         $count = count($input['array']);
         $set_depth = 0;
         $set_depth_modifier = false;

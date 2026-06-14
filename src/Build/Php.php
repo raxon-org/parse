1cbbccdad48be2545122d0776b7882afc0f41651
Remco van der Velde <remco@universeorange.com>
2026-06-14 18:47:29 +0200

    newtest

--- src/Build/Php.php
+++ src/Build/Php.php
@@ -1845,7 +1845,7 @@ class Php {
                             $token = Php::document_tag_prepare($object, $flags, $options, $token);
                             $embed = Php::document_tag($object, $flags, $options, $token);
                             $is_raw = $object->config('package.raxon/parse.build.state.is_raw');
-                            d($is__raw);
+                            d($is_raw);
                             if(property_exists($options, 'variable')){
                                 if($variable_old){{
                                     $data[] = $variable_old . '[] = "' . $before_text . '";';

946dd6539258522a2c8ee7b62b3fb58ff4d3ebfa
Remco van der Velde <remco@universeorange.com>
2026-06-15 16:49:04 +0200

    newtest

--- src/Module/Token.php
+++ src/Module/Token.php
@@ -825,6 +825,7 @@ class Token
                                         $record
                                     );
                                     d($list);
+                                    breakpoint('yes');
                                     $variable_target = Token::variable_name($object, $flags, $options, $variable_name);
                                     $variable_explode = explode($variable_target, $variable_name, 2);
                                     $cast = Token::cast_get($object, $flags, $options, $variable_explode[0]);

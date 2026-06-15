e0a5bbdf1cbee76ed9416212fe60be419ebf416f
Remco van der Velde <remco@universeorange.com>
2026-06-15 17:53:58 +0200

    newtest

--- src/Build/Php.php
+++ src/Build/Php.php
@@ -1725,7 +1725,7 @@ class Php {
                             $before_text = str_replace('\\&', $ampersand_uuid, $before_text);
                             $before_text = str_replace('&quot;', $double_quote_uuid, $before_text);
                             $before_text = str_replace('&apos;', $single_quote_uuid, $before_text);
-                            d($before_text);
+                            breakpoint($before_text);
                             $after_text = substr($record['text'], $has_second_double_quote_backslash + 1);
                             $after_text = str_replace('\\&', $ampersand_uuid, $after_text);
                             $after_text = str_replace('&quot;', $double_quote_uuid, $after_text);

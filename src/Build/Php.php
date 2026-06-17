ef4d187edf6f669c6fecce2edd44c4831b25de1f
Remco van der Velde <remco@universeorange.com>
2026-06-17 23:44:27 +0200

    init

--- src/Build/Php.php
+++ src/Build/Php.php
@@ -1848,6 +1848,7 @@ class Php {
                         array_key_exists('method', $record)
                     ){
                         $method = Php::method($object, $flags, $options, $record, $before, $after);
+                        d($before);
                         if(!empty($before)){
                             $separator = $object->config('package.raxon/parse.build.state.separator');
                             $data[] = 'try {';

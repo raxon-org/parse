65258836cd2c6c20a8e372857d37f44f10b616da
Remco van der Velde <remco@universeorange.com>
2026-06-18 00:01:43 +0200

    init

--- src/Build/Php.php
+++ src/Build/Php.php
@@ -1851,6 +1851,7 @@ class Php {
                         if(!empty($before)){
                             $separator = $object->config('package.raxon/parse.build.state.separator');
                             $data[] = 'try { //t1';
+                            d($record);
                             d($before);
                             foreach($before as $line){
                                 if($separator === null){

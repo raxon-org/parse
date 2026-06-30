6d62515633b42ceea92484ade1da021f0d440b09
Remco van der Velde <remco@universeorange.com>
2026-06-30 10:52:06 +0200

    init

---
+++ src/Plugin/App/App_Controller_Configure.Controller.Host.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * @package Plugin\Modifier
+ * @author Remco van der Velde
+ * @since 2025-02-11
+ * @license MIT
+ * @version 1.0
+ * @changeLog
+ *    - all
+ */
+namespace Raxon\Parse\Plugin\App;
+
+use Exception;
+use Raxon\App as Framework;
+
+trait App_Controller_Configure {
+
+    /**
+     * @throws Exception
+     */
+    protected function app_controller_configure(string $caller): void
+    {
+        ddd($caller);
+    }
+}

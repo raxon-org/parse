9f27dbf1ecc8dc7ee599c63f954e2aa2b2ce5c54
Remco van der Velde <remco@universeorange.com>
2026-09-17 15:36:05 +0200

    init

--- src/Build/Php.php
+++ src/Build/Php.php
@@ -677,6 +677,7 @@ class Php {
                         $marker_data[$record['line']] = [];
                         $marker_data[$record['line']][] = $record['marker']['value']['array'][2] ?? [];
                         $object->config('package.raxon/parse.build.state.is_raw', true);
+                        breakpoint($data);
                         $quote = array_pop($data);
                         $marker_data = Php::document_tag($object, $flags, $options, $marker_data);
                         foreach($marker_data as $line){

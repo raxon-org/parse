bfa6be4db45ea48b35587bfbb22a511e93da92b9
Remco van der Velde <remco@universeorange.com>
2026-09-18 10:42:00 +0200

    init

--- src/Build/Php.php
+++ src/Build/Php.php
@@ -1901,6 +1901,7 @@ class Php {
                             $data[] = $method . ';';
                         }
                         elseif($method) {
+                            breakpoint($method);
                             $uuid_method = Core::uuid_variable();
                             $data[] = 'try { //t2';
                             $data[] = $uuid_method . ' = ' . $method . ';';

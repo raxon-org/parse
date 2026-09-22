47c5e7dc1975451549faef3fb0fed0d1a49a0168
Remco van der Velde <remco@universeorange.com>
2026-09-22 13:43:30 +0200

    init

--- src/Plugin/Debug/Debug.Exception.Read.php
+++ src/Plugin/Debug/Debug.Exception.Read.php
@@ -10,13 +10,24 @@
  */
 namespace Plugin;
 
+use Raxon\Exception\ObjectException;
+use Raxon\Module\Core;
+
 trait Debug_Exception_Read {
+    /**
+     * @throws ObjectException
+     */
     function debug_exception_read(mixed $return_type='array'): mixed
     {
         $object = $this->object();
         $url = $object->config('controller.dir.data') . '/' .
             'Debug.Exception' . $object->config('extension.json');
-        ddd($url);
+        $data = $object->read($url);
+        if($data !== null){
+            $result =  $data->get('Debug.Exception');
+            $result = Core::object($result, $return_type);
+            return $result;
+        }
         return [];
     }
 
